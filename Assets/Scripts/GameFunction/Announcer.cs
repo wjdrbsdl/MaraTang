639dad0bb80091147fe72c8adfb7dec82c8c0736
YiYop <wjdrbsdl1011@naver.com>
2024-02-03 03:04:47 +0900

    [add] 아나운서 클래스 추가

---
+++ Assets/Scripts/GameFunction/Announcer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Announcer
+{
+    public static Announcer Instance;
+
+    public Announcer()
+    {
+        Instance = this;
+    }
+
+    public void TestInstance()
+    {
+        Debug.Log("테스트 완료");
+    }
+
+    public void AnnounceState(string message)
+    {
+
+    }
+}
