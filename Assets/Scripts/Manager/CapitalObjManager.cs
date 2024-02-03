f4a82b30ba0516314c89373b880f122d92cce12a
YiYop <wjdrbsdl1011@naver.com>
2024-02-03 22:38:17 +0900

    [add] 자원 오브젝트 관리자 추가

---
+++ Assets/Scripts/Manager/CapitalObjManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapitalObjManager : MgGeneric<CapitalObjManager>
+{
+    //캐피탈 스프라이트 렌더러 풀 관리자
+    [SerializeField]
+    private ObjTokenCapital m_sample; 
+    private Stack<ObjTokenCapital> m_capitalObjStack = new Stack<ObjTokenCapital>();
+    private void Awake()
+    {
+        g_instance = this;
+    }
+
+    public void RequestObjToken(TokenTile _tile)
+    {
+        ObjTokenCapital capitalObj = Instantiate(m_sample).GetComponent<ObjTokenCapital>();
+        capitalObj.gameObject.transform.SetParent(_tile.GetObject().transform);
+        capitalObj.gameObject.transform.localPosition = new Vector3(0, 0);
+    }
+
+}
