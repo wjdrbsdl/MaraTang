d8700f0c52aa2f1c855d19053a151e18fe3b72d4
YiYop <wjdrbsdl1011@naver.com>
2024-02-06 16:14:15 +0900

    [add] 환경설정 추가

---
+++ Assets/Scripts/Manager/MgSetting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MgSetting : MgGeneric<MgSetting>
+{
+    public enum ButtonType
+    {
+        다시하기, 앱종료, 사운드조정, 언어선택
+    }
+
+    public float m_SoundValue;
+
+    public void QuitApp()
+    {
+#if UNITY_EDITOR
+        Debug.Log("게임 종료");
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void RestartGame()
+    {
+        Application.Quit();
+    }
+}
