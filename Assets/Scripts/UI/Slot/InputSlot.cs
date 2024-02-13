8f3b3ed7ed1e61f7a2a2c415429334bdd2470670
YiYop <wjdrbsdl1011@naver.com>
2024-02-13 12:17:49 +0900

    inputslot 주석

--- Assets/Scripts/UI/Slot/InputSlot.cs
+++ Assets/Scripts/UI/Slot/InputSlot.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 using TMPro;
 public class InputSlot : SlotBase
 {
-    public TMP_InputField tmpInput;
-    public TMP_Text selectedText;
-    private int m_clicked = 0;
+    public TMP_InputField tmpInput; //수량 입력부분
+    public TMP_Text selectedText; //나중에 아이콘으로 대체될부분
     public int min, max = 1;
 
     private void Start()
@@ -15,10 +14,10 @@ public class InputSlot : SlotBase
 
     public override void OnLeftClick()
     {
-        //1. 보유한 재료 전시 -> 선택하면 해당 inputSlot의 item으로 세팅
+        //1. 보유한 재료 전시 요청 
         MgUI.GetInstance().ShowCaseOpen(this);
-        //2. min,max 값 조정 
-   
+        //2. 쇼케이스에서 선택한 재료로 InputSlot 세팅
+  
     }
 
     public void SelectItem(string _itemName)
