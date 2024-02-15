aa3688ccf9426c47c88b72b6f661f10a5fc1c575
YiYop <wjdrbsdl1011@naver.com>
2024-02-16 02:16:00 +0900

    [add] 이펙트관련 변수 추가 이펙트 효과, 그 타입등

--- Assets/Scripts/Token/TokenEffect.cs
+++ Assets/Scripts/Token/TokenEffect.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public enum EffectProperty
 {
-   RestEffectCount, RestActionTurn, RestWoldTurn, ApplyTiming 
+  RestEffectCount, RestActionTurn, RestWoldTurn, ApplyTiming, Power1, Power2
 }
+
 public class TokenEffect : TokenBase
 {
 
@@ -20,4 +21,32 @@ public class TokenEffect : TokenBase
             Debug.Log("남은 효과 없음");
     }
 
+    public void UseTurn(bool _isActionTurn, int useCount = 1)
+    {
+        if (_isActionTurn)
+        {
+            CalStat(EffectProperty.RestActionTurn, - useCount);
+        }
+        //월드턴이면
+        else
+        {
+            CalStat(EffectProperty.RestWoldTurn, - useCount);
+        }
+        
+        if(IsRestTurn() == false)
+        {
+            //제거하기
+        }
+
+    }
+
+    public bool IsRestTurn()
+    {
+        if(GetStat(EffectProperty.RestActionTurn) == 0 || GetStat(EffectProperty.RestWoldTurn) == 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
