5745553a22c9223d7477bb3a8f39d7ca39f979d1
YiYop <wjdrbsdl1011@naver.com>
2024-02-15 21:49:21 +0900

    [add] 이펙트 토큰 추가 1. 임시로 사용될만한 속성값 정함.

---
+++ Assets/Scripts/Token/TokenEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public enum EffectStat
+{
+   RestEffectCount, RestActionTurn, RestWoldTurn, ApplyTiming 
+}
+public class TokenEffect : TokenBase
+{
+
+    TokenEffect()
+    {
+        m_tokenIValues = new int[GameUtil.EnumLength(EffectStat.RestEffectCount)];
+    }
+
+    public void Effect()
+    {
+        if (GetStat(EffectStat.RestEffectCount) == 0)
+            Debug.Log("남은 효과 없음");
+    }
+
+}
