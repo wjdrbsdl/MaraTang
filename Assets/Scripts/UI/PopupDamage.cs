fc1e84a6741ac808be787b8f0f1edb2fa92f3453
YiYop <wjdrbsdl1011@naver.com>
2024-02-19 17:50:17 +0900

    [add] 데미지 mesh 사용 1. mesh Object 생성 prefab 적용 2. sort main 적용

--- Assets/Scripts/UI/PopupDamage.cs
+++ Assets/Scripts/UI/PopupDamage.cs
@@ -7,17 +7,25 @@ public class PopupDamage : MgGeneric<PopupDamage>
 {
     // Start is called before the first frame update
     public DamageNumber damagePopup;
+    public DamageNumberMesh damageMesh;
     public float scale = 1f;
-
+    public float upPos = 2f;
     private void Start()
     {
         InitiSet();
     }
 
-    public void DamagePop(GameObject _object, int _deal)
+    public void DamagePop(GameObject _object, int _deal, int a)
     {
         DamageNumber num = damagePopup.Spawn(_object.transform.position, _deal);
         num.transform.SetParent(gameObject.transform);
+        num.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    public void DamagePop(GameObject _object, int _deal)
+    {
+        DamageNumber num = damageMesh.Spawn(_object.transform.position+ Vector3.up*upPos, _deal);
+       
         num.transform.localScale = new Vector3(scale, scale, scale);
     }
 }
