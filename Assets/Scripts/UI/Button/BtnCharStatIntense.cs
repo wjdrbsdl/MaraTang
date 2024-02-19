e1bd6c7b8099b31c4cce472b33b3b63dea5bcf63
YiYop <wjdrbsdl1011@naver.com>
2024-02-19 18:22:15 +0900

    [add] 캐릭스텟 표기용 버튼 추가

---
+++ Assets/Scripts/UI/Button/BtnCharStatIntense.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+
+public class BtnCharStatIntense : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text m_statText;
+    private TokenChar m_char;
+    public void SetButton(TokenChar _char, CharStat _stat)
+    {
+        m_char = _char;
+        m_statText.text = _char.GetStat(_stat).ToString();
+    }
+    public void OnButtonClick()
+    {
+        //해당 액션을 선택하면 
+        if(m_char.IsPlayerChar())
+        Debug.Log("스텟 강화하기");
+        else
+        {
+            Debug.Log("내캐릭불가");
+        }
+    }
+}
