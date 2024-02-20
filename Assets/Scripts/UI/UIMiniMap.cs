ac4c562dbca477fb220be2674a9ee3615351f591
YiYop <wjdrbsdl1011@naver.com>
2024-02-21 03:09:47 +0900

    [add] 미니맵용 유아이 추가 1. 클릭시 클릭위치 체크

---
+++ Assets/Scripts/UI/UIMiniMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMiniMap : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //정해진 영역에서 어느 포인터에 찍혔는지 
+        Debug.Log(eventData.position);
+    }
+
+}
