e4dbd831eb743316c50189b62252a138f9ba9e96
YiYop <wjdrbsdl1011@naver.com>
2024-02-23 17:45:47 +0900

    [add] eventToken 생성자 추가 1. 마스터 데이터본 2. 복사본

--- Assets/Scripts/Token/TokenEvent.cs
+++ Assets/Scripts/Token/TokenEvent.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class TokenEvent : TokenBase
 {
-   
+    public TokenEvent()
+    {
+
+    }
+
+    //마스터데이터 생성
     public TokenEvent(int pid, int value2)
     {
         m_tokenPid = pid;
+    }
+    
+    //복사본 생성
+
+    public TokenEvent(TokenEvent _masterToken)
+    {
+        m_tokenPid = _masterToken.m_tokenPid;
     }
 }
