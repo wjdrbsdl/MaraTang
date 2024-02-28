d17c043c5d1b0f8b5176e93f4767ddc45bf60a08
YiYop <wjdrbsdl1011@naver.com>
2024-02-29 00:18:21 +0900

    [add] 숨김 타일 구름으로 대체

--- Assets/Scripts/GameFunction/HideTile.cs
+++ Assets/Scripts/GameFunction/HideTile.cs
@@ -6,14 +6,23 @@ public class HideTile : MonoBehaviour
 {
     [SerializeField]
     private SpriteMask m_mask;
+    public Sprite[] m_sprites;
+    public SpriteRenderer m_render;
+    private void Start()
+    {
+        int ran = Random.Range(0, m_sprites.Length);
+        m_render.sprite = m_sprites[ran];
+    }
 
     public void MaskOn()
     {
-        m_mask.backSortingOrder = 3;
+    //    m_mask.backSortingOrder = 3;
     }
 
     public void FogOff()
     {
-        m_mask.backSortingOrder = 2;
+    //    m_mask.backSortingOrder = 2;
+
+        gameObject.SetActive(false);
     }
 }
