19618c3c4ff9a9d9e5db87082e9b2b7e45bae399
YiYop <wjdrbsdl1011@naver.com>
2024-04-22 04:44:43 +0900

    [add] 컨텐츠 마스터데이터 클래스 추가

---
+++ Assets/Scripts/Manager/DataManager/ContentData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ContentData
+{
+    //컨텐츠 데이터로 퀘스트 조건, 페널티, 보상안등의 정보를 담고 있음. 
+    public int ContentPid;
+    public EOrderType CondtionType;
+    public List<TOrderItem> MainItemList;
+    public List<TOrderItem> SubItemList;
+
+    public ContentData(string[] _parsingData)
+    {
+
+    }
+}
