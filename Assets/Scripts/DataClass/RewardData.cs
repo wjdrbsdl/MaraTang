07da2e018fd75d3235eeb94d3e2c32aa4b6891c1
YiYop <wjdrbsdl1011@naver.com>
2024-05-07 15:38:15 +0900

    [add] 컨텐트 정보의 리워드 타입에 따라 오더 타입 추출

--- Assets/Scripts/DataClass/RewardData.cs
+++ Assets/Scripts/DataClass/RewardData.cs
@@ -24,14 +24,28 @@ public class RewardData : IOrderCustomer
 
     public RewardData(ContentData _contentData, int _chunkNum = MGContent.NO_CHUNK_NUM) //어떠한 데이트가 들어오면 거기에 맞게 보상 설정. 
     {
+        //1. 컨텐트 정보에서 리워드 타입을 가져오고
         RewardType = _contentData.RewardType;
         if (RewardType.Equals(ERewardType.None))
             return;
 
+        //2. 리워드 타입에 오더 타입을 변경
         EOrderType orderType = EOrderType.None;
         switch (RewardType)
         {
             //리워드 타입에 따라서 OrderType 맞춰서 세팅 
+            
+            case ERewardType.CharStat:
+            case ERewardType.Capital:
+            case ERewardType.ActionToken:
+                orderType = EOrderType.ItemSelect;
+                break;
+            case ERewardType.EventToken:
+                orderType = EOrderType.SpawnEvent;
+                break;
+            case ERewardType.CharToken:
+                orderType = EOrderType.SpawnMonster;
+                break;
         }
 
         OrderMaker orderMaker = new();
