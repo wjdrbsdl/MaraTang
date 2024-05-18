dda2f398aab350e08b8cd874790d1757eaed64e6
YiYop <wjdrbsdl1011@naver.com>
2024-05-18 19:13:00 +0900

    [add] 국가 클래스 추가

---
+++ Assets/Scripts/DataClass/Nation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Nation
+{
+    private TokenTile m_capital;
+    private List<TokenTile> m_territorryList;
+
+    public void AddTerritory(TokenTile _tileToken)
+    {
+        //이미 있는 영토
+        if(m_territorryList.IndexOf(_tileToken)>= 0)
+                return;
+
+        m_territorryList.Add(_tileToken);
+    }
+
+
+}
