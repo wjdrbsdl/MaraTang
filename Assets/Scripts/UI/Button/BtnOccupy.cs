162d8e6240af3ac71b8e83732d74d233fd907774
YiYop <wjdrbsdl1011@naver.com>
2024-05-19 03:02:45 +0900

    [add] 점령용 버튼 추가

---
+++ Assets/Scripts/UI/Button/BtnOccupy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnOccupy : MonoBehaviour
+{
+    private TokenTile m_tile;
+
+    public void SetButton(TokenTile _tile)
+    {
+        m_tile = _tile;
+    }
+    public void OnClickOccupy()
+    {
+        PlayerManager.GetInstance().SelectOccupyTile(m_tile);
+    }
+
+    public void SetActive(bool _on)
+    {
+        gameObject.SetActive(_on);
+    }
+}
