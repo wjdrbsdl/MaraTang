cfe35271661b647df840ac9a2fb4d2cc6cfab071
YiYop <wjdrbsdl1011@naver.com>
2024-06-15 01:38:03 +0900

    [add] 국가 테크 클래스 생성자

--- Assets/Scripts/DataClass/NationTechTree.cs
+++ Assets/Scripts/DataClass/NationTechTree.cs
@@ -10,6 +10,17 @@ public enum TechTreeStat
 
 public class NationTechTree
 {
+    private int m_techPid;
+    private string m_techName;
+    private int m_techClass;
+    private int[] m_techValues;
 
-
+    public NationTechTree(List<int[]> matchCode, string[] valueCode)
+    {
+        m_techPid = int.Parse(valueCode[0]);
+        m_techName = valueCode[1];
+        m_techClass = (int.Parse(valueCode[2]));
+        m_techValues = new int[GameUtil.EnumLength(TechTreeStat.Class)];
+        GameUtil.InputMatchValue(ref m_techValues, matchCode, valueCode);
+    }
 }
