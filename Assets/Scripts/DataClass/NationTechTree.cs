a9e07e608516a85a7ce8b0165a3f8b94bce282ea
YiYop <wjdrbsdl1011@naver.com>
2024-06-15 01:30:33 +0900

    [add] 국가 기술 트리 클래스 추가

---
+++ Assets/Scripts/DataClass/NationTechTree.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TechTreeStat
+{
+    Class, NeedTurn, NeedWood, NeedMineral
+}
+
+public class NationTechTree
+{
+
+
+}
