77b632a5fd12a71916fa7cce85361daad466fe3b
YiYop <wjdrbsdl1011@naver.com>
2024-06-18 19:47:01 +0900

    [add] 코스트 지불을 위한 인터페이스 추가

---
+++ Assets/Scripts/DataClass/ITradeCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ITradeCustomer
+{
+
+    public void CheckInventory(List<(TokenType,int,int)> _costList);
+    public void PayInventory(List<(TokenType, int, int)> _costList);
+}
