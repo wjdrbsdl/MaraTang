74fca52beeaa7a95921bc00291a0316e02eb2519
YiYop <wjdrbsdl1011@naver.com>
2024-06-19 22:49:56 +0900

    [add] 테크클래스를 관리할 테크 매니저 추가 1. 해당 매니저를 통해 우선도에 부합하는 테크 찾기등 2. 국가정책에 따라 알맞은 techPid 반환받기

---
+++ Assets/Scripts/DataClass/TechTreeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class TechTreeManager
+{
+    //국가에서 테크 발전 계획을 수립하기 위해 우선도에 따라 남은 계획을 정렬하여 반환하는곳
+    private List<NationTechTree> techTree;
+
+    public TechTreeManager()
+    {
+        techTree = MgMasterData.GetInstance().GetTechDic().Select(dic => dic.Value).ToList(); //딕셔너리의 nationTech만 가지고 리스트로 전환
+        
+    }
+
+    public int GetTechPidByNotDone(List<int> _doneTech)
+    {
+        for (int i = 0; i < techTree.Count; i++)
+        {
+            int techPid = techTree[i].GetPid();
+            
+            //배웠던거라면 넘기고
+            if (_doneTech.IndexOf(techPid) >= 0)
+                continue;
+
+            //아니라면
+            return techPid; //해당 기술의 pid를 반환
+        }
+
+        return FixedValue.No_INDEX_NUMBER; //배울게 없으면 없는 인덱스 반환
+    }
+
+    public int GetTechPidByClass(int _class, List<int> _doneTech)
+    {
+        for (int i = 0; i < techTree.Count; i++)
+        {
+            int treeClass = techTree[i].GetTechValue(TechTreeStat.Class);
+            //다른 클래스면 넘기고
+            if (treeClass.Equals(_class) == false)
+                continue;
+            //배웠던거라면 넘기고
+            if (_doneTech.IndexOf(treeClass) >= 0)
+                continue;
+
+            //아니라면
+            return techTree[i].GetPid(); //해당 기술의 pid를 반환
+        }
+
+        return FixedValue.No_INDEX_NUMBER; //배울게 없으면 없는 인덱스 반환
+    }
+}
