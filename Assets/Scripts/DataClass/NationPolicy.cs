4732fc27b7c33a1fbc739418c4c3d0a3fffb637f
YiYop <wjdrbsdl1011@naver.com>
2024-06-25 01:02:45 +0900

    [add] 정책 유보 카운트 사용처 추가

--- Assets/Scripts/DataClass/NationPolicy.cs
+++ Assets/Scripts/DataClass/NationPolicy.cs
@@ -47,6 +47,10 @@ public class NationPolicy
     public void Remind()
     {
         //국가 턴이 종료 될 때 해당 안건을 되돌아보며 턴 진행, 필요 턴 진행했을때 조건 고려, 취소, 변경등을 고려. 
+        if (m_holdPolicyCount >= 3)
+        {
+            //해당 정책 포기해볼까. 
+        }
     }
 
     public void Done()
