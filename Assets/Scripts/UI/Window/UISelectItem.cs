2621a056d3f9bd9c88406582f9fffd7bce0fdbb1
YiYop <wjdrbsdl1011@naver.com>
2024-06-26 23:23:08 +0900

    [add] SelectUi 추가 1. 셀렉 인포 Class를 매개변수로 UI Setting을 호출받음 2. 호출받은 아이템리스트를 SelectSlot으로 표기 3. SelectSlot으로 선택시 SelectCustomer 콜백 받음 4. 콜백받으면 셀렉인포에 받은 정보를 기록 5. 갱신된 기록으로 다시 정보 표기

--- Assets/Scripts/UI/Window/UISelectItem.cs
+++ Assets/Scripts/UI/Window/UISelectItem.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class UISelectItem : UIBase
+public class UISelectItem : UIBase, ISelectCustomer
 {
     //해당 플레이어가 지닌 자원을 종류 수량을 선택하는 부분. 
     [SerializeField]
     private SelectSlot m_showSlotSample;
+    [SerializeField]
+    private Transform m_grid;
+    [SerializeField]
+    private SelectSlot[] m_selectSlots;
+    private SelectItemInfo m_selectInfo;
+  
+    public void SetSelectedInfo(SelectItemInfo _selectInfo)
+    {
+        m_selectInfo = _selectInfo;
+        //1. 아이템 수만큼 showSlot을 생성
+        MakeSamplePool<SelectSlot>(ref m_selectSlots, m_showSlotSample.gameObject, m_selectInfo.ShowItemList.Count, m_grid);
+        //2. 리스트대로 슬랏에 표기
+        SetSlots();
+        //3. 선택한 아이템들은 따로 추가 표기
+        SetSelectSlots();
+    }
+
+    private void SetSlots()
+    {
+        for (int i = 0; i < m_selectInfo.SelectedItem.Count; i++)
+        {
+            TOrderItem orderItem = m_selectInfo.SelectedItem[i]; //정보 표기할 아이템 
+            ISelectCustomer customer = this;
+            int index = i;
+            m_selectSlots[i].SetSlot(orderItem, customer, index);
+        }
+    }
+
+    private void SetSelectSlots()
+    {
+        for (int i = 0; i < m_selectInfo.SelectedIndex.Count; i++)
+        {
+            int slotIndex = m_selectInfo.SelectedIndex[i];
+            m_selectSlots[slotIndex].SetSelectState();
+        }
+    }
+
+    public void OnSelectCallBack(int _slotIndex)
+    {
+        m_selectInfo.AddChooseItem(_slotIndex);
+    }
 
 }
+
