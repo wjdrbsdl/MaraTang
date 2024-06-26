3e29e7bc16f56446fd6cff073c891737902cce6f
YiYop <wjdrbsdl1011@naver.com>
2024-06-26 23:48:21 +0900

    [fix] 셀렉슬랏에서 콜백시 슬랏인덱스 전달

--- Assets/Scripts/UI/Slot/SelectSlot.cs
+++ Assets/Scripts/UI/Slot/SelectSlot.cs
@@ -26,6 +26,6 @@ public class SelectSlot : SlotBase
     public override void OnLeftClick()
     {
         base.OnLeftClick();
-        callBackCustomer.OnSelectCallBack();
+        callBackCustomer.OnSelectCallBack(slotIndex);
     }
 }
