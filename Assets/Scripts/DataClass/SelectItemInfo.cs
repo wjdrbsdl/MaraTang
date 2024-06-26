38976d8e444fb66a42a8f45f1e2f98b27a502c5b
YiYop <wjdrbsdl1011@naver.com>
2024-06-26 23:19:50 +0900

    [add] 선택위한 클래스 SelectItemInfoClass  추가 1. List<torderItem>으로 선택리스트 변수 2. 선택 index를 전달받으면 SeelctIndexList에 저

---
+++ Assets/Scripts/DataClass/SelectItemInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectItemInfo
+{
+    public List<TOrderItem> ShowItemList; //보여주려는 아이템리스트
+    public List<TOrderItem> SelectedItem; //선택한 아이템  
+    public List<int> SelectedIndex;
+    public SelectItemInfo(List<TOrderItem> _showList)
+    {
+        //만들려는 아이템 리스트를가지고 클래스 생성
+        ShowItemList = _showList;
+        SelectedItem = new List<TOrderItem>();
+        SelectedIndex = new List<int>();
+    }
+
+    public void ChooseItem(TOrderItem _item)
+    {
+        if (SelectedItem.IndexOf(_item) > 0)
+        {
+            SelectedItem.Remove(_item);
+            return;
+        }
+        SelectedItem.Add(_item);
+    }
+    public void AddChooseItem(int _itemIndex)
+    {
+        if (SelectedIndex.IndexOf(_itemIndex) > 0)
+        {
+            SelectedIndex.Remove(_itemIndex);
+            return;
+        }
+        SelectedIndex.Add(_itemIndex);
+    }
+
+}
