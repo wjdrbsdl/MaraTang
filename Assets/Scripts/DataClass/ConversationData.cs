fbdb5a6cc454b77aaea2d3519540d696c44491cb
YiYop <wjdrbsdl1011@naver.com>
2024-07-02 17:36:23 +0900

    [add] 대사 관련 클래스 추가 1. 대사 UI 표기할 UIConversation 2. 대사 데이터 관련 클래스 ConversationData

---
+++ Assets/Scripts/DataClass/ConversationData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationData
+{
+    private int Pid; //대사 식별번호
+    private string ScriptData; //대사
+    private int ReceiveAnswer; //플레이어가 선택한 값
+
+    public ConversationData(int _pid, string _script)
+    {
+        Pid = _pid;
+        ScriptData = _script;
+    }
+
+}
---
+++ Assets/Scripts/UI/Window/UIConversation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIConversation : UIBase
+{
+    //해당 타일의 정보 표기 및 타일에서 할 수 있는 작업을 제공하는 UI
+    [SerializeField]
+    private Image m_talkerImage;
+    [SerializeField]
+    private TMP_Text m_talkerText;
+    private CharStat[] m_showStat = {CharStat.Strenth, CharStat.Dexility, CharStat.Inteligent };
+
+    public void SetConversation(TokenChar _char)
+    {
+        Switch(true);
+  
+    }
+}
