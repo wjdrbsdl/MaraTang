fe0fe71fd7ebd26b011794370081515fe5de2785
YiYop <wjdrbsdl1011@naver.com>
2024-07-16 14:53:35 +0900

    [add] 타일작업용 마크핀 추가

--- Assets/Scripts/Manager/MgNaviPin.cs
+++ Assets/Scripts/Manager/MgNaviPin.cs
@@ -7,7 +7,7 @@ public class MgNaviPin : MgGeneric<MgNaviPin>
     public NaviPin m_pinSample;
     public int m_pinMaxCount = 3;
     private List<NaviPin> m_pinLIst = new();
-
+    private List<NaviPin> m_tileWorkPinList = new();
     public NaviPin RequestNaviPin()
     {
         if (m_pinMaxCount <= m_pinLIst.Count)
@@ -19,6 +19,13 @@ public class MgNaviPin : MgGeneric<MgNaviPin>
         NaviPin naviPin = Instantiate(m_pinSample);
         m_pinLIst.Add(naviPin);
         return naviPin;
+    }
+
+    public void RequestTileWorkPin(TokenTile _pinPosTile, int _workType)
+    {
+        NaviPin naviPin = Instantiate(m_pinSample);
+        m_tileWorkPinList.Add(naviPin);
+        naviPin.SetPinInfo(_pinPosTile.GetObject().gameObject.transform.position, _workType);
     }
 
     public void RemovePin(NaviPin _pin)
