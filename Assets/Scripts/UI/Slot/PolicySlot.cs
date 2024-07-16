e4141aa639aa5165bf421b3086945954d0080bfc
YiYop <wjdrbsdl1011@naver.com>
2024-07-16 23:50:16 +0900

    [add] 정책 표기 Ui에서 삭제 제안 버튼 추가

--- Assets/Scripts/UI/Slot/PolicySlot.cs
+++ Assets/Scripts/UI/Slot/PolicySlot.cs
@@ -8,11 +8,18 @@ public class PolicySlot : SlotBase
     public TMP_Text m_policyName;
     public TMP_Text m_policyState;
     public TMP_Text m_policyNum;
+    private NationPolicy m_nationPolicy;
 
     public void SetSlot(NationPolicy _nationPolicy)
     {
         m_policyName.text = _nationPolicy.GetMainPolicy().ToString();
         m_policyState.text = _nationPolicy.GetHoldCount().ToString();
         m_policyNum.text = _nationPolicy.GetPlanIndex().ToString();
+        m_nationPolicy = _nationPolicy;
+    }
+
+    public void OnClickCancleBtn()
+    {
+        MgNation.GetInstance().GetNation(m_nationPolicy.GetNaionNum()).SuggestPolicyCancle(m_nationPolicy);
     }
 }
