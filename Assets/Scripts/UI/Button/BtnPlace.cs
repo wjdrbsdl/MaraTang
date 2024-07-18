d7a3afa292017dcc13d61a1cd1672db367040968
YiYop <wjdrbsdl1011@naver.com>
2024-07-18 19:19:00 +0900

    [add] 장소 버튼 세팅 수정 1. 장소명 표기

--- Assets/Scripts/UI/Button/BtnPlace.cs
+++ Assets/Scripts/UI/Button/BtnPlace.cs
@@ -7,10 +7,12 @@ public class BtnPlace : MonoBehaviour
 {
     private TileType m_tileType;
     private UIBase m_motherUI;
+    [SerializeField] private TMP_Text m_placeText;
 
     public void SetButton(TileType _tileType, UIBase _motherUI)
     {
         m_tileType = _tileType;
+        m_placeText.text = m_tileType.ToString();
         m_motherUI = _motherUI;
     }
     public void OnClickPlaceEnter()
