28c0c4a023e696e49308655a821ccd3367938adf
YiYop <wjdrbsdl1011@naver.com>
2024-07-18 19:07:30 +0900

    [add] 입장 장소 표기할 버튼 생성

---
+++ Assets/Scripts/UI/Button/BtnPlace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnPlace : MonoBehaviour
+{
+    private TileType m_tileType;
+    private UIBase m_motherUI;
+
+    public void SetButton(TileType _tileType, UIBase _motherUI)
+    {
+        m_tileType = _tileType;
+        m_motherUI = _motherUI;
+    }
+    public void OnClickPlaceEnter()
+    {
+        Debug.Log(m_tileType + "으로 입장");
+    }
+
+    public void SetActive(bool _on)
+    {
+        gameObject.SetActive(_on);
+    }
+}
