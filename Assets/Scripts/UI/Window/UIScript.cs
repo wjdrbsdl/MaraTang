92cdbf5470612670644b365d7fd71698cc5bde02
YiYop <wjdrbsdl1011@naver.com>
2024-08-07 16:26:24 +0900

    [수정] 스크립트 퀘스트 전달 전에 초기화 1. 초기화를 나중에 하면 퀘스트 진행으로 다시 스크립트 필요하게 되는데, 이 스크립트를 남은 초기화 진행으로 인해 종료 시켜버림.

--- Assets/Scripts/UI/Window/UIScript.cs
+++ Assets/Scripts/UI/Window/UIScript.cs
@@ -34,11 +34,14 @@ public class UIScript : UIBase
     public override void ReqeustOff()
     {
         //취소 요청을 받았을 때 얘는 취소 안되는데 일단은 취소되는걸로 테스트
-        if (m_select != null)
-            m_select.Cancle();
-
         Switch(false);
-        m_select = null;
+        SelectItemInfo scriptInfo = m_select; //미리 받아놓고
+        m_select = null; //초기화 진행
+
+        //1. 먼저 취소를 진행해버리면 취소 진행으로 다시 이 대화창이 SetScript로 진행이됨. 그리고 위의 스위치 오프나 null이 진행되어 버그발생
+        //2. 그래서 먼저 초기화후 취소 진행 
+        if (scriptInfo != null)
+            scriptInfo.Cancle();
 
     }
 }
