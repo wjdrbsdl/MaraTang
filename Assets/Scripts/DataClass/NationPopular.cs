3f3d2cb7551db84b40a61fb66c2c57af9f06fdc6
YiYop <wjdrbsdl1011@naver.com>
2024-10-02 22:17:12 +0900

    [추가] 굶은사람 페널티 1. 사망으로 진행

--- Assets/Scripts/DataClass/NationPopular.cs
+++ Assets/Scripts/DataClass/NationPopular.cs
@@ -7,7 +7,7 @@ public class NationPopular
 {
     private Nation m_nation;
     private int m_mealRatio = 10; //필요 식량 비율 - 식량 효율이 좋을수록 값 감소
-
+    private int hungPeople = 0;
     public NationPopular(Nation _nation)
     {
         m_nation = _nation;
@@ -17,23 +17,26 @@ public class NationPopular
     {
         //1. 현재 인구수 만큼 식량 소비 진행 
         ExpandMeal();
+        //2. 굶은 사람에 대한 페널티 적용
+        PenaltyHungry();
     }
 
     private void ExpandMeal()
     {
         int population = m_nation.GetResourceAmount(Capital.Person);
         int meal = m_nation.GetResourceAmount(Capital.Food);
-        int ablePopulation = AbleEatPopulation();
-        if (ablePopulation < population)
+        int ablePeople = AbleEatPopulation();
+        hungPeople = 0; //굶는사람 0
+        if (ablePeople < population)
         {
             //식량이 부족하면 먹일수 있는 식량 만큼 식량 감소
-            int ableMeal = ablePopulation * m_mealRatio;
+            int ableMeal = ablePeople * m_mealRatio;
             m_nation.CalResourceAmount(Capital.Food, -ableMeal);
             Debug.Log("기존 식량" + meal +"식후 식량"+m_nation.GetResourceAmount(Capital.Food));
-            int hungryPopulation = population - ablePopulation; //먹지 못한 인간들
-            Debug.Log(hungryPopulation + "인구가 먹지 못함");
+            hungPeople = population - ablePeople; //먹지 못한 인간들
+            Debug.Log(hungPeople + "인구가 먹지 못함");
         }
-        else if(population <= ablePopulation)
+        else if(population <= ablePeople)
         {
             //식량이 풍족하면 현재 인원수만큼 식량 감소
             int needMeal = population * m_mealRatio;
@@ -45,6 +48,19 @@ public class NationPopular
     private int AbleEatPopulation()
     {
         return m_nation.GetResourceAmount(Capital.Food) / m_mealRatio;
+    }
+
+    private void PenaltyHungry()
+    {
+        if (hungPeople == 0)
+        {
+            Debug.Log("굶은사람 없음");
+            return;
+        }
+            
+
+        m_nation.CalResourceAmount(Capital.Person, -hungPeople);
+        Debug.Log("굶은사람 사망" + hungPeople);
     }
 
 }
