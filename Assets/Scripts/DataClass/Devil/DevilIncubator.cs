9e2bceaf5e258d37e37b8c8785bb597a57dd240d
YiYop <wjdrbsdl1011@naver.com>
2024-10-08 16:41:17 +0900

    [추가] 악마 폴더, 악마 인큐베이터 1. 등장 악마 선택, 부활 담당할 클래스

---
+++ Assets/Scripts/DataClass/Devil/DevilIncubator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public enum DevilState
+{
+    봉인, 유아, 성인, 토벌
+}
+
+public class DevilIncubator
+{
+    private List<TokenChar> m_devilList;
+    private List<DevilState> m_devilStateList;
+
+    public void DiceDevilList()
+    {
+        //등장할 악마를 골라서 생성 
+        Dictionary<int,TokenChar> charDic = MgMasterData.GetInstance().GetCharDic();
+        List<TokenChar> devilList = new();
+        foreach(KeyValuePair<int, TokenChar> tokenChar in charDic)
+        {
+            if (tokenChar.Value.GetCharType().Equals(CharType.Devil))
+            {
+                Debug.Log(tokenChar.Value.GetItemName() + " 악마 리스트에 추가");
+                devilList.Add(tokenChar.Value);
+            }
+        }
+    }
+
+}
+
