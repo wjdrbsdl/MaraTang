515f29223a2025bdb24ff0574fd2c60cbc6d59d0
YiYop <wjdrbsdl1011@naver.com>
2024-10-14 21:40:07 +0900

    [추가] 악마 발생 턴 주기 상세화 1. 최초 안전 대비 턴 추가 2. 발생 후 활동기간 추가 3. 토벌시 보너스 턴 추가

--- Assets/Scripts/DataClass/Devil/DevilIncubator.cs
+++ Assets/Scripts/DataClass/Devil/DevilIncubator.cs
@@ -16,9 +16,24 @@ public class DevilIncubator
     private List<TokenTile> m_birthTile = new();
     private List<bool> m_contentMeet = new();
     private List<DevilState> m_devilStateList = new();
-    private int m_turnTerm = 15; //발생 주기
+    private int m_firstTerm = 30; //카운터 최초 시작 주기
+    private int m_baseActiveTerm = 30; //활동 시간 - 해당 시간 동안 발생, 봉인, 전투등이 진행
+    private int m_nextTerm = 20;  //악마의 활동이나 토벌, 봉인등이 종료된 후 다음 활동기까지 준비 시간
+    private int m_birthRestTurn = 0; //부활까지 남은 시간 -- 턴 조건
     private bool m_turnEnough = true;
-    private int birthCount = 0;
+    private int birthCount = 0; //부활시킨 수
+
+    private enum BirthCountType
+    {
+      First,  Birth, Victory
+    }
+
+    #region 초기화
+    public DevilIncubator()
+    {
+        SetNextBirthTurn(BirthCountType.First); //첫 탄생 주기로 남은 부활턴 설정
+    }
+
     public void SetBirthRegion(List<int> _chunkList, List<TokenTile> _tileList)
     {
         m_chunkNum = _chunkList;
@@ -55,7 +70,9 @@ public class DevilIncubator
             //Debug.Log(m_devilList[i].GetItemName() + "악마 확정");
         }
     }
+    #endregion
 
+    #region 턴에 따른 작업
     public void ChangeWorldContent(int _pid, bool _result)
     {
         //해당 퀘스트 컨텐츠 결과값이 들어왔을 때 영향을 받는 악마에게 조건 적용
@@ -63,13 +80,23 @@ public class DevilIncubator
 
     public void ChangeWorldTurn(int _currentTurn)
     {
-        if(_currentTurn%m_turnTerm == 0)
-        {
-            m_turnEnough = true;
-            Debug.Log("때 조건 충족");
-        }
+        CountBirthRestTurn();
     }
 
+    private void CountBirthRestTurn()
+    {
+        //카운터가 진행중이였으면 카운터하고 탄생 체크
+        if(0 < m_birthRestTurn)
+        {
+            m_birthRestTurn -= 1;
+
+            if (m_birthRestTurn == 0)
+                m_turnEnough = true;
+        }
+    }
+    #endregion
+
+    #region 악마 탄생, 토벌
     public void BirthDevil()
     {
         Debug.LogWarning("악마 부활 조건 체크 후 부활진행");
@@ -83,8 +110,6 @@ public class DevilIncubator
             return;
         }
 
-        //개별 조건 확인해서 되는 애 중에 랜덤으로 생성
-        int randomDevil = Random.Range(0, m_devilList.Count);
         //악마 수대로 랜덤수 뽑기 - 기존에 뽑은 애는 넘겨야 하므로 
         List<int> randomOrder = GameUtil.GetRandomNum(m_devilList.Count, m_devilList.Count);
 
@@ -103,7 +128,36 @@ public class DevilIncubator
             //뽑았으면 종료
             break;
         }
-        
+        SetNextBirthTurn(BirthCountType.Birth); //다음 부활주기 재세팅
     }
+
+    //악마 토벌시 호출
+    public void RemoveDevil()
+    {
+        SetNextBirthTurn(BirthCountType.Victory); 
+    }
+
+    //다음 발생주기 턴 정하는 부분
+    private void SetNextBirthTurn(BirthCountType _birthType)
+    {
+        switch (_birthType)
+        {
+            //기존 악마 토벌이나 봉인으로 승리했을땐, 남은 주기의 3분의1에 다음 준비기간을 추가
+            case BirthCountType.Victory:
+                m_birthRestTurn = m_birthRestTurn / 3 + m_nextTerm;
+                break;
+            //악마가 탄생했을때는 해당 악마 활동 주기 만큼 다음 악마 도래까지 설정
+            case BirthCountType.Birth:
+                m_birthRestTurn = m_baseActiveTerm;
+                break;
+            case BirthCountType.First:
+                m_birthRestTurn = m_firstTerm;
+                break;
+        }
+
+    }
+    #endregion
+
+
 }
 
