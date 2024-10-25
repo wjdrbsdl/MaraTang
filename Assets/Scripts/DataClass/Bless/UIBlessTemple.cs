d4059dbdb1a70d9e08efd56c5a010d4f87275aa9
YiYop <wjdrbsdl1011@naver.com>
2024-10-25 21:53:15 +0900

    [추가] 신전 UI

---
+++ Assets/Scripts/DataClass/Bless/UIBlessTemple.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class UIBlessTemple : MonoBehaviour
+{
+    //모시는 신에 따라 은총을 갈구할 수 있다.
+    //보유한 은총을 장착해제 가능한 곳 
+
+    public void PleaseBlessBtn()
+    {
+        MgGodBless.GetInstance().PleaseBless();
+    }
+}
