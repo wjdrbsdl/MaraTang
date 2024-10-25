073ef76fb0ef367465516959e28b7a51364d7a78
YiYop <wjdrbsdl1011@naver.com>
2024-10-26 00:47:10 +0900

    [추가] 장비 클래스

---
+++ Assets/Scripts/DataClass/Equipment/EquiptItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EquiptItem
+{
+    public TOrderItem m_effect; //플레이어 스텟에 가하는 요소
+
+    public EquiptItem()
+    {
+        m_effect = new TOrderItem(TokenType.CharStat, (int)CharStat.MaxHp, 30);
+    }
+}
