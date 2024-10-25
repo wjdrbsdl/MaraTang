47726eaf0f7e08c00f497f3c03e6ba7d29212677
YiYop <wjdrbsdl1011@naver.com>
2024-10-26 00:50:07 +0900

    [추가] 플레이어 장비클래스 추가

---
+++ Assets/Scripts/DataClass/Equipment/PlayerEquipment.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerEquipment
+{
+    public List<EquiptItem> m_haveList;
+    public List<EquiptItem> m_equipList;
+    public static PlayerEquipment g_instnace;
+
+    public PlayerEquipment()
+    {
+        g_instnace = this;
+        m_haveList = new();
+    }
+
+    public void AddEquipment(EquiptItem _equipment)
+    {
+        Debug.Log("보유 은총 추가");
+        m_haveList.Add(_equipment);
+    }
+
+    public void Equipt(EquiptItem _equipment)
+    {
+       
+    }
+}
