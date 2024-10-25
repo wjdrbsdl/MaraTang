6af4ba49b250e58a79b75abf982f46076dc6c151
YiYop <wjdrbsdl1011@naver.com>
2024-10-26 00:37:47 +0900

    [추가]플레이어 블레스 관리 클래스

---
+++ Assets/Scripts/DataClass/Bless/PlayerBless.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerBless 
+{
+    public List<GodBless> m_haveList;
+    public static PlayerBless g_instnace;
+
+    public PlayerBless()
+    {
+        g_instnace = this;
+        m_haveList = new();
+    }
+
+    public void AddBless(GodBless _bless)
+    {
+        Debug.Log("보유 은총 추가");
+        m_haveList.Add(_bless);
+    }
+}
