2236956259cb7302dba0f86655a21f6533c94edf
YiYop <wjdrbsdl1011@naver.com>
2024-10-25 21:52:39 +0900

    [수정] 매니저 싱글톤

--- Assets/Scripts/DataClass/Bless/MgGodBless.cs
+++ Assets/Scripts/DataClass/Bless/MgGodBless.cs
@@ -2,13 +2,13 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class MgGodBless : MonoBehaviour
+public class MgGodBless : MgGeneric<MgGodBless>
 {
     //은총 데이터 모아놓고
     //신전으로부터 은총 요구를 받으면 적당한 은총을 하사
     private List<GodBless> m_blessList = new();
 
-    void Start()
+    public override void ReferenceSet()
     {
         for (int i = 0; i < 5; i++)
         {
