b0d14669cdd1dabde75ec6586ead7572eb7dbe7d
YiYop <wjdrbsdl1011@naver.com>
2024-10-28 18:29:31 +0900

    [추가] 신전이 속한 종교에 따라 은총 요구 전달

--- Assets/Scripts/DataClass/Bless/UIBlessTemple.cs
+++ Assets/Scripts/DataClass/Bless/UIBlessTemple.cs
@@ -6,7 +6,7 @@ public class UIBlessTemple : UIBase
 {
     //모시는 신에 따라 은총을 갈구할 수 있다.
     //보유한 은총을 장착해제 가능한 곳 
-
+    private BlessMainCategory m_curTempleClass = BlessMainCategory.무;
 
     public void SetTempleInfo(TokenTile _tile)
     {
@@ -16,7 +16,7 @@ public class UIBlessTemple : UIBase
 
     public void PleaseBlessBtn()
     {
-       GodBless bless =  MgGodBless.GetInstance().PleaseBless();
+       GodBless bless =  MgGodBless.GetInstance().PleaseBless(BlessMainCategory.무);
         Debug.Log(bless.m_effect.Tokentype + "에 영향 주는 은총 받음");
         PlayerBless.g_instnace.AddBless(bless);
     }
