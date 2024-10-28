76b6c78d9eb176018acc56625e26f8dd6497ef6d
YiYop <wjdrbsdl1011@naver.com>
2024-10-28 20:25:13 +0900

    [추가] 신이 내릴수 있는 은총 파싱 구조

--- Assets/Scripts/DataClass/Bless/God.cs
+++ Assets/Scripts/DataClass/Bless/God.cs
@@ -8,8 +8,9 @@ public class God
 {
     public int PID;
     public int Tier;
+    public string Test;
     public BlessMainCategory m_mainCategory = BlessMainCategory.무; //해당 신의 분류
-    public List<GodBless> m_blessList; //해당 신이 보유한 블레스
+    public List<int> m_blessList; //해당 신이 보유한 블레스
 
     public God(string[] _parseStr)
     {
@@ -27,9 +28,19 @@ public class God
         {
             m_mainCategory = BlessMainCategory.궁사;
         }
+     
         Tier = int.Parse(_parseStr[3]);
 
-       // Debug.Log(_parseStr[1] + "피아이디" + PID + " 카테고리" + m_mainCategory + " 티어" + Tier);
+        string[] haveBless = _parseStr[4].Split(FixedValue.PARSING_LIST_DIVIDE);
+        m_blessList = new();
+        for(int i = 0; i < haveBless.Length; i++)
+        {
+            int blessPid = int.Parse(haveBless[i]);
+            m_blessList.Add(blessPid);
+          //  Debug.Log(_parseStr[1] + "이 내릴수 있는 가호 pid " + blessPid);
+        }
+
+        // Debug.Log(_parseStr[1] + "피아이디" + PID + " 카테고리" + m_mainCategory + " 티어" + Tier);
     }
-    
+
 }
