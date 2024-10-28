ab640548345a522e7d8416b642df38702f14d888
YiYop <wjdrbsdl1011@naver.com>
2024-10-28 23:28:20 +0900

    [추가] 시너지 우선 확인해야하는 수준으로 정렬

--- Assets/Scripts/DataClass/Bless/BlessSynerge.cs
+++ Assets/Scripts/DataClass/Bless/BlessSynerge.cs
@@ -20,12 +20,14 @@ public class BlessSynerge
         Name = _divdeValues[1];
         m_needBlessList = new();
         GameUtil.ParseOrderItemList(m_needBlessList, _divdeValues[2]);
+        //서브 인덱스가 클수록 더 좁은 범위 먼저 확인해야하므로 앞으로 배치 
+        m_needBlessList.Sort((TOrderItem A, TOrderItem B) => B.SubIdx.CompareTo(A.SubIdx));
         //for (int i = 0; i < m_needBlessList.Count; i++)
         //{
         //    TOrderItem blessItem = m_needBlessList[i];
         //    Debug.Log(Name + "시너지 필요한 조건은 " + (BlessSynergeCategoryEnum)blessItem.SubIdx + "중 " + blessItem.Value + "계열");
         //}
-        
+
     }
 
 }
