987f681ee66618b211e204db8e189b61a3f42de6
YiYop <wjdrbsdl1011@naver.com>
2024-10-28 19:07:48 +0900

    [추가] 파싱데이터로 God 인스턴스 생성

--- Assets/Scripts/DataClass/Bless/God.cs
+++ Assets/Scripts/DataClass/Bless/God.cs
@@ -6,11 +6,30 @@ using UnityEngine;
 
 public class God
 {
+    public int PID;
+    public int Tier;
     public BlessMainCategory m_mainCategory = BlessMainCategory.무; //해당 신의 분류
     public List<GodBless> m_blessList; //해당 신이 보유한 블레스
 
-    public God()
+    public God(string[] _parseStr)
     {
-      
+        PID = int.Parse(_parseStr[0]);
+        string className = _parseStr[2];
+        if(className == "전사")
+        {
+            m_mainCategory = BlessMainCategory.전사;
+        }
+        else if (className == "법사")
+        {
+            m_mainCategory = BlessMainCategory.마법;
+        }
+        else if (className == "궁사")
+        {
+            m_mainCategory = BlessMainCategory.궁사;
+        }
+        Tier = int.Parse(_parseStr[3]);
+
+        Debug.Log(_parseStr[1] + "피아이디" + PID + " 카테고리" + m_mainCategory + " 티어" + Tier);
     }
+    
 }
