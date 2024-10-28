b668c66a3bb21ed046f829b682b3f22e5eb25f26
YiYop <wjdrbsdl1011@naver.com>
2024-10-28 18:28:05 +0900

    [추가] 신 클래스

---
+++ Assets/Scripts/DataClass/Bless/God.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class God
+{
+    public BlessMainCategory m_mainCategory = BlessMainCategory.무; //해당 신의 분류
+    public List<GodBless> m_blessList; //해당 신이 보유한 블레스
+
+    public God()
+    {
+      
+    }
+}
