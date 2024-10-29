b8acb7725805fe7d2bceb88539e79ee8902d85d9
YiYop <wjdrbsdl1011@naver.com>
2024-10-30 08:39:33 +0900

    [추가] 외부건설후 작업서 제거

--- Assets/Scripts/UI/Button/BtnBuild.cs
+++ Assets/Scripts/UI/Button/BtnBuild.cs
@@ -31,20 +31,22 @@ public class BtnBuild : MonoBehaviour
 
     public void MakeOutBuildWork()
     {
+       // Debug.Log("외부건설 작업서 만듬");
         //해당 타일 변경 건축물 건설 진행 
         List<TOrderItem> nothing = new();
         TokenTile tile = m_tile;
         TileType buildType = m_tileType;
 
-        WorkOrder order = new WorkOrder(nothing, 100, (int)buildType, WorkType.InterBuild);
+        WorkOrder order = new WorkOrder(nothing, 100, (int)buildType, WorkType.ChangeBuild);
         //다른 타일로 작업시 m_tile이 변경될수있으므로 다른 인스턴스로 생성
         Action doneEffect = delegate
         {
-            tile.ChangePlace(buildType);
+            tile.DoneOutBuild(buildType);
+            tile.RemoveWork(order);
         };
         order.SetDoneEffect(doneEffect);
         m_tile.RegisterWork(order);
-        m_tileInfoUI.ResetSetPlace();
+        m_tileInfoUI.ResetUI();
     }
 
     public void SetActive(bool _on)
