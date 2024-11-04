10660126c5ac2cd2119f71336077823fddca1562
YiYop <wjdrbsdl1011@naver.com>
2024-11-04 21:08:02 +0900

    [추가] 국가 외곽 타일 반환

--- Assets/Scripts/DataClass/ContentPlay/TileReturner.cs
+++ Assets/Scripts/DataClass/ContentPlay/TileReturner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -7,8 +8,25 @@ public class TileReturner
     public TokenTile NationBoundaryTile()
     {
         TokenTile tile = null;
+        Nation nation =  MgNation.GetInstance().GetNation(0); //임시로 0번째 나라
+        List<TokenTile> nationTiles = nation.GetTerritorry();
+
+        for (int i = 0; i < nationTiles.Count; i++)
+        {
+           List<TokenTile> roundTile = GameUtil.GetTileTokenListInRange(1, nationTiles[i].GetXIndex(), nationTiles[i].GetYIndex(), 1);
+            for (int r = 0; r < roundTile.Count; r++)
+            {
+                //주변 타일의 넘버가 같은 국가가 하나라도 국가가 아니면 얘는 외각
+                int tileInRange = roundTile[r].GetStat(TileStat.Nation);
+                if(tileInRange == FixedValue.NO_NATION_NUMBER)
+                {
+                    tile = nationTiles[i];
+                    return tile;
+                }
+            }
+        }
         //국가 영역 중 경계선에 있는 타일 하나를 반환 
         //이를 토대로 국가 주변에 어떠한 것들을 스폰시킴. 
-        return tile;
+        return null;
     }
 }
