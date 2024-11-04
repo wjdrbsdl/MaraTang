4f8a3d667bf559caa8ba5643617f372e2f8c9732
YiYop <wjdrbsdl1011@naver.com>
2024-11-04 22:55:21 +0900

    [추가]타일액션 클래스

---
+++ Assets/Scripts/Token/TokenTileAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[System.Serializable]
+public class TokenTileAction : TokenBase
+{
+    [SerializeField]
+    public bool IsAutoStart = false;
+
+    #region 액션 토큰 : 생성부분 추후 테이블 파싱 값으로 생성하기
+ 
+    public TokenTileAction(List<int[]> matchCode, string[] valueCode)
+    {
+        m_tokenPid = int.Parse(valueCode[0]);
+        m_itemName = valueCode[1];
+        IsAutoStart = (int.Parse(valueCode[2]) != 0);
+        
+    }
+    #endregion
+}
