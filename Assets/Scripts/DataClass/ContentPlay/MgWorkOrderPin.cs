7195e3575d12ecb07958e68698bf22ef309a5ca3
YiYop <wjdrbsdl1011@naver.com>
2024-11-07 18:31:45 +0900

    [추가] 작업오더핀 매니저와 작업오더핀 클래스

---
+++ Assets/Scripts/DataClass/ContentPlay/MgWorkOrderPin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class MgWorkOrderPin : MgGeneric<MgWorkOrderPin>
+{
+    public WorkOrderPin m_pinSample;
+    private List<WorkOrderPin> m_pinLIst = new();
+    
+    public WorkOrderPin RequestWorkOrderPin(TokenTile _tile)
+    {
+        WorkOrderPin workPin = Instantiate(m_pinSample);
+        workPin.SetPinInfo(_tile);
+        m_pinLIst.Add(workPin);
+        return workPin;
+    }
+
+    public void RemovePin(WorkOrderPin _pin)
+    {
+        //제거 요구 받은 핀이 null이면 종료
+        if (_pin == null)
+            return;
+
+        m_pinLIst.Remove(_pin);
+        Destroy(_pin.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/DataClass/ContentPlay/WorkOrderPin.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class WorkOrderPin : MonoBehaviour
+{
+    public Vector2 naviPoint; //꽂혀있는 곳 
+    public SpriteRenderer m_spriteRender;
+    private Camera m_mainCam;
+    private void Start()
+    {
+        m_mainCam = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Navigate();
+    }
+
+    public void SetPinInfo(TokenTile _tile)
+    {
+        naviPoint = new Vector2(_tile.GetXIndex(), _tile.GetYIndex());
+    }
+
+    public void SwitchPin(bool _on)
+    {
+        gameObject.SetActive(_on);
+    }
+
+    public void DestroyPin()
+    {
+        Destroy(gameObject);
+    }
+
+    #region 핀 조정
+    private void Navigate()
+    {
+        //카메라 뷰에 들어와있으면
+        if (IsInCamevaView())
+        {
+            transform.position = naviPoint;
+            return;
+        }
+
+        SwitchPin(false); //화면밖이면 꺼
+    }
+
+    Vector2 screenPoint;
+    private bool IsInCamevaView()
+    {
+        screenPoint = m_mainCam.WorldToViewportPoint(naviPoint);
+        bool isIn = screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        // Debug.Log(isIn + " "+screenPoint);
+        return isIn;
+    }
+    #endregion
+}
