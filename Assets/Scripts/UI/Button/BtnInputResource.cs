1069e64dd23027a7c0f971f49a62d13ce977b554
YiYop <wjdrbsdl1011@naver.com>
2024-11-11 21:19:11 +0900

    [추가] 작업오더에 자원 넣기 호출

---
+++ Assets/Scripts/UI/Button/BtnInputResource.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnInputResource : MonoBehaviour
+{
+    private TokenTile m_tile;
+    private UITileInfo m_motherUI;
+
+    public void SetButton(TokenTile _tile, UITileInfo _motherUI)
+    {
+        m_tile = _tile;
+        m_motherUI = _motherUI;
+    }
+    public void OnClickPut()
+    {
+        WorkOrder workOrder = m_tile.GetWorkOrder();
+        if (workOrder == null)
+            return;
+
+        workOrder.PutResource(PlayerCapitalData.g_instance);
+
+        m_motherUI.ResetUI();
+    }
+
+    public void SetActive(bool _on)
+    {
+        gameObject.SetActive(_on);
+    }
+}
