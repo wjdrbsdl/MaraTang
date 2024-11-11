2cff7c450c276aa001f6022819fa75ac0f046b7c
YiYop <wjdrbsdl1011@naver.com>
2024-11-11 21:33:30 +0900

    [수정] 워크핀 관리 변수 1. 리스트에서 딕션너리로 바꿔서 워크오더에 맞게 핀을 할당

--- Assets/Scripts/DataClass/ContentPlay/MgWorkOrderPin.cs
+++ Assets/Scripts/DataClass/ContentPlay/MgWorkOrderPin.cs
@@ -5,20 +5,23 @@ using UnityEngine;
  public class MgWorkOrderPin : MgGeneric<MgWorkOrderPin>
 {
     public WorkOrderPin m_pinSample;
-    private List<WorkOrderPin> m_pinLIst = new();
+    private Dictionary<WorkOrder, WorkOrderPin> m_pinDic = new();
     
     public WorkOrderPin RequestWorkOrderPin(TokenTile _tile)
     {
         Debug.Log("작업핀 요청 받음");
         WorkOrderPin workPin = Instantiate(m_pinSample);
         workPin.SetPinInfo(_tile);
-        m_pinLIst.Add(workPin);
+        m_pinDic.Add(_tile.GetWorkOrder(), workPin);
         return workPin;
     }
 
     public void RemovePin(TokenTile _tile)
     {
-
+        Debug.Log("작업핀 제거 요청 받음");
+        WorkOrderPin pin = m_pinDic[_tile.GetWorkOrder()];
+        pin.DestroyPin();
+        m_pinDic.Remove(_tile.GetWorkOrder());
     }
 
 }
