10aa6127fd4d47a24be607958ab040b5f9b28911
YiYop <wjdrbsdl1011@naver.com>
2024-11-11 20:45:27 +0900

    [추가] 타일 서칭 클래스

---
+++ Assets/Scripts/DataClass/ContentPlay/SearchTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SearchTile
+{
+    public void FindSomething(TokenTile _tile)
+    {
+        //타일에서 무언갈 찾아서 반환 혹은 플레이어 재산에 포함시키는 곳 
+        Debug.Log("타일에서 뭔가좀 찾아봄");
+    }
+}
