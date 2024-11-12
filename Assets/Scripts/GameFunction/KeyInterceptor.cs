a172841e27277aa9b66b6c479238084dcf089030
YiYop <wjdrbsdl1011@naver.com>
2024-11-12 22:42:37 +0900

    [추가] 키인터셉터시 이전 인터셉터 기록함수 추가

--- Assets/Scripts/GameFunction/KeyInterceptor.cs
+++ Assets/Scripts/GameFunction/KeyInterceptor.cs
@@ -12,4 +12,7 @@ public interface KeyInterceptor
     public void DoubleClickTokenBase(TokenBase _tokenBase);
 
     public void PushNumKey(int _keyNum);
+
+    //이전 키 사용자 기록 - 키 반환 할때 이전 사용자로 돌림- 이전사용자는 다시 이전사용자로 돌리기 
+    public void SetPreIntecoptor(KeyInterceptor _ceptor);
 }
