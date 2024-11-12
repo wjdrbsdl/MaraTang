fa7d3c86eeb087419d29efc0478e78ff457fd29b
YiYop <wjdrbsdl1011@naver.com>
2024-11-12 22:07:42 +0900

    [추가] 인풋 결과 전달받을 인터셉터 인터페이스 추가

---
+++ Assets/Scripts/GameFunction/KeyInterceptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface KeyInterceptor
+{
+    public void ClickTokenBase(TokenBase _tokenBase);
+
+    public void DoubleClickTokenBase(TokenBase _tokenBase);
+
+    public void PushNumKey(int _keyNum);
+}
