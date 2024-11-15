303f4f069f64dc7512e0aa0bd010ba36bb6a0d88
YiYop <wjdrbsdl1011@naver.com>
2024-11-15 21:22:16 +0900

    [추가] 노동코인 클래스 1. 각 타일의 효과를 발휘 하기 위해선 최소 코인 1개가 있어야함 2. 타일이 피해를 입었을 때 코인을 확률적으로 잃을 수 있음.

---
+++ Assets/Scripts/InGame/Nation/LaborCoin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LaborCoin
+{
+    public int Pid = 0; //할당된 pid
+    public int[] Pos; //할당된 일자리
+    public int NationNum;
+
+    public LaborCoin(int _pid, Nation _nation)
+    {
+        Pid = _pid;
+        NationNum = _nation.GetNationNum();
+        SetPos( _nation.GetCapital().GetMapIndex());
+    }
+
+    public void GoWork(TokenTile _tile)
+    {
+        SetPos(_tile.GetMapIndex());
+    }
+
+    public void BackCapital()
+    {
+        SetPos(MgNation.GetInstance().GetNation(NationNum).GetCapital().GetMapIndex());
+    }
+
+
+    private void SetPos(int[] _pos)
+    {
+        Pos = _pos;
+    }
+}
