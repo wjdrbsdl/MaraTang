6dafa6199975646a5a9ef7fc3f401b7d4c96dfbd
YiYop <wjdrbsdl1011@naver.com>
2024-11-19 21:57:17 +0900

    [추가] 국가 사건 관리 클래스

---
+++ Assets/Scripts/InGame/Nation/NationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NationEvent
+{
+    private Nation m_nation;
+
+    public NationEvent(Nation _nation)
+    {
+        m_nation = _nation;
+    }
+
+    public void WatchEvent()
+    {
+
+    }
+
+  
+}
