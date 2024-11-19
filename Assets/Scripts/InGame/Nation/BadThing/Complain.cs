9315a98da66aceafabba3c113715648a272a8e74
YiYop <wjdrbsdl1011@naver.com>
2024-11-19 18:57:26 +0900

    [추가] 민원 클래스, 폴더

---
+++ Assets/Scripts/InGame/Nation/BadThing/Complain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Complain
+{
+    public string Name;
+    public string Type; // 컴플레인의 부류 - 자원 요구, 특정 스텟 요구치로 단순 확률싸움
+    public List<TOrderItem> NeedItems; // 필요한것들
+    public int RestTurn; //인내기간
+    public List<TOrderItem> FailEffect; //실패시 어쩔지
+    public List<TOrderItem> SuccesEffect; //성공시 어쩔지
+
+    public bool React()
+    {
+        //컴플레인에 대응하기
+        TokenChar _char = PlayerManager.GetInstance().GetMainChar();
+        switch (Type)
+        {
+            case "자원요구":
+                //돈 계산
+                return PlayerCapitalData.g_instance.CheckInventory(new TItemListData(NeedItems));
+            case "스텟요구":
+                //스텟으로 확률 계산
+                return true;
+        }
+        return false;
+    }
+
+    public void Effect(bool _isSuccess)
+    {
+        if (_isSuccess)
+        {
+
+        }
+    }
+
+    public void DoSucces()
+    {
+
+    }
+
+    public void DoFail()
+    {
+
+    }
+}
+
