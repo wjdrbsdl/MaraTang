862313c2f9f3c826958f0cfbc7d228bcc912d157
YiYop <wjdrbsdl1011@naver.com>
2024-11-19 21:47:11 +0900

    [추가] 민원 관리자 1. 민원 분류 2. 해당 타일에서 발생가능한 민원 반환

---
+++ Assets/Scripts/InGame/Nation/BadThing/ComplainManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ComplainManager
+{
+    public List<Complain> m_complinLIst; //민원 종류들
+
+    public void ClassfyComplain()
+    {
+        //마스터데이터로 부터 민원 종류들 가져와서 분류 
+
+    }
+
+   public Complain OccurComplain(TokenTile _tile)
+    {
+        Nation nation = _tile.GetNation();
+        //해당 장소에서 발생가능한 민원과 그 확률을 다른 스텟과 조율하여 민원 반환
+        Complain occurComplain = null;
+        //분류된 민원을 가지고, 새로운 민원으로 복사해서 반환 
+
+        return occurComplain;
+    }
+}
+
