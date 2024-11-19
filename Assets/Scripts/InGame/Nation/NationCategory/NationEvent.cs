b9bd0954a615862b5121e810cefc9528b8377127
YiYop <wjdrbsdl1011@naver.com>
2024-11-19 22:20:03 +0900

    [추가] 컴플매니저를 통해 해당 타일에서 발생가능한 민원 요구

--- Assets/Scripts/InGame/Nation/NationCategory/NationEvent.cs
+++ Assets/Scripts/InGame/Nation/NationCategory/NationEvent.cs
@@ -17,10 +17,14 @@ public class NationEvent
     public void WatchEvent()
     {
         List<TokenTile> territory = m_nation.GetTerritorry();
+        ComplainManager compleMg = ComplainManager.GetInstance();
         //Debug.Log(m_nation.GetNationNum() + " 사건 발생 유무 확인");
         for (int i = 0; i < territory.Count; i++)
         {
             TokenTile tile = territory[i];
+            Complain complain = compleMg.OccurComplain(tile);
+            if (complain == null)
+                Debug.Log("발생 민원 없음");
                 
         }
     }
