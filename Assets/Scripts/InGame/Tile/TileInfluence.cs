ef7e56a488567b5755ed91747de8884cc1fc74d2
YiYop <wjdrbsdl1011@naver.com>
2024-11-19 21:47:34 +0900

    [추가] 타일 부가효과 클래스 1. 격려, 열정, 나태 등 해당 타일에서 국소적으로 적용될 영향들.

---
+++ Assets/Scripts/InGame/Tile/TileInfluence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TileInfluence
+{
+    //의욕 상승
+    //능률 하강 등 해당 타일에 국소적으로 적용될 요소들. 
+    public int m_restTurn; //유지시간
+    public TOrderItem m_effect; //적용효과
+}
