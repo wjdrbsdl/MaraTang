4b7844b5633d652ffb15d0ded670dc106239377c
YiYop <wjdrbsdl1011@naver.com>
2024-11-20 22:38:58 +0900

    [추가] 단일선택 로직 1. 선택한 아이템대로 적용 2. 적용시 아이템 리스트 리셋하여 재진행

--- Assets/Scripts/InGame/Selector/OneBySelectInfo.cs
+++ Assets/Scripts/InGame/Selector/OneBySelectInfo.cs
@@ -17,6 +17,78 @@ public class OneBySelectInfo
     public ITradeCustomer Taker; //받는자
     public List<TOrderItem> ItemList; //남은 선택 리스트
 
+    public OneBySelectInfo(List<TOrderItem> _selectList)
+    {
+        ItemList = _selectList;
+    }
 
+    public void OpenSelectUI()
+    {
+        //열고
+    }
+
+    private TokenType[] exception = {TokenType.Equipt };
+    public void SelectOne(TOrderItem _item)
+    {
+        //적용할것 골랐다.
+        Debug.Log("고름");
+        //적용시키고
+        bool isAdapt = AdaptItem(_item);
+        
+        if (isAdapt == false)
+        {
+            //해당 아이템이 적용 안되었으면 선택은 무효 
+            return;
+        }
+            
+
+        RemoveItem(_item);
+        //선택한 수 까고
+        restSelectCount -= 1;
+        
+        if(CheckRest() == true)
+        {
+            //남은게 있으면 다시 UI 세팅
+            return;
+        }
+        //아니면 UI 종료
+            
+    }
+
+    private bool AdaptItem(TOrderItem _item)
+    {
+        Debug.Log("적용");
+        TokenChar mainChar = PlayerManager.GetInstance().GetMainChar();
+        switch (_item.Tokentype)
+        {
+            case TokenType.Capital:
+                Debug.Log("자원올린다");
+                Taker.PayCostData(new TItemListData(_item), false);
+                return true; //따로 받은게 없음
+            case TokenType.Equipt:
+                Debug.Log("장비장착한다");
+                //이부분 장착가능하면 바로 진행하고
+                //불가능하면 다시 교환 UI를 진행해야하는건 
+                //케릭터 장비쪽에서 진행 
+                
+                break;
+            case TokenType.CharStat:
+                Debug.Log("스텟올린다");
+                mainChar.CalStat((CharStat)_item.SubIdx, _item.Value);//형변환 안해두되는데 아쉽군. 
+                break;
+        }
+        //적용 항목이 없는 경우엔 적용 안된걸로
+        return false;
+    }
+
+    private void RemoveItem(TOrderItem _item)
+    {
+        ItemList.Remove(_item);
+    }
+
+    private bool CheckRest()
+    {
+        return restSelectCount >= 1;
+    }
 
 }
