a7b938d0c463c13ee4098b425875d0d47455fdd2
YiYop <wjdrbsdl1011@naver.com>
2024-11-20 23:03:15 +0900

    [추가] 장비 클래스 복사

--- Assets/Scripts/InGame/Equipment/EquiptItem.cs
+++ Assets/Scripts/InGame/Equipment/EquiptItem.cs
@@ -10,6 +10,7 @@ public enum EquiptEnum
 public class EquiptItem
 {
     private int m_pid;
+    private string m_name;
     public List<TOrderItem> m_effect = new(); //플레이어 스텟에 가하는 요소
 
     public EquiptItem()
@@ -19,7 +20,20 @@ public class EquiptItem
 
     public EquiptItem(List<int[]> matchCode, string[] valueCode)
     {
+        m_pid = int.Parse(valueCode[0]);
+        m_name = valueCode[1];
 
+    }
+
+    public EquiptItem(EquiptItem _masterData)
+    {
+        m_pid = _masterData.m_pid;
+        m_name = _masterData.m_name;
+        for (int i = 0; i < _masterData.m_effect.Count; i++)
+        {
+            TOrderItem effect = _masterData.m_effect[i];
+            m_effect.Add(effect);
+        }
     }
 
     public int GetPid()
