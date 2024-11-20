16a13700a745dc2e3c7e59c97713f375c67bba9a
YiYop <wjdrbsdl1011@naver.com>
2024-11-20 21:08:52 +0900

    [추가] 토큰타입enum에 Equipt

--- Assets/Scripts/Token/TokenBase.cs
+++ Assets/Scripts/Token/TokenBase.cs
@@ -17,7 +17,7 @@ public enum OnChangeEnum
 public enum TokenType
 {
     Tile, Char, CharStat, Action, Player, Capital, Event, Nation, None, Content, NationTech, Conversation, NationStat, OnChange, Bless,
-    MonsterNationSpawn
+    MonsterNationSpawn, Equipt
 }
 
 public class TokenBase
