937e76e928b212ebe2ab255cfda21ae45a4b05ba
YiYop <wjdrbsdl1011@naver.com>
2024-11-20 23:39:50 +0900

    [추가] 하나씩 선택지용 UI, Slot 클래스

---
+++ Assets/Scripts/InGame/Selector/UIOneByeSelect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIOneByeSelect : UIBase
+{
+    //해당 플레이어가 지닌 자원을 종류 수량을 선택하는 부분. 
+    [SerializeField]
+    private OneSelectSlot m_showSlotSample;
+    [SerializeField]
+    private Transform m_grid;
+    [SerializeField]
+    private OneSelectSlot[] m_selectSlots;
+    private OneBySelectInfo m_onebyselectInfo;
+
+    //SelectItemInfo 정보값을 표기, SelectSlot으로부터 입력을 받으며 진행. 
+    #region 슬랏으로 정보 표기
+    public void SetSelectedInfo(OneBySelectInfo _selectInfo)
+    {
+        UISwitch(true);
+        m_onebyselectInfo = _selectInfo;
+        //1. 아이템 수만큼 showSlot을 생성
+        MakeSamplePool<OneSelectSlot>(ref m_selectSlots, m_showSlotSample.gameObject, m_onebyselectInfo.ItemList.Count, m_grid);
+        //2. 리스트대로 슬랏에 표기
+        SetSlots();
+    }
+
+    public void OnSelectItem(int _index)
+    {
+        //아이템리스트에 대응되는 슬롯이 클릭된 경우 index가 전달됨
+
+    }
+
+    private void SetSlots()
+    {
+        int itemCount = m_onebyselectInfo.ItemList.Count;
+      
+        for (int i = 0; i < itemCount; i++)
+        {
+            TOrderItem orderItem = m_onebyselectInfo.ItemList[i]; //정보 표기할 아이템 
+            //ISelectCustomer customer = this;
+            int index = i;
+            
+            m_selectSlots[i].gameObject.SetActive(true);
+            m_selectSlots[i].SetSlot(orderItem, this, index);
+        }
+        for (int i = itemCount; i < m_selectSlots.Length; i++)
+        {
+            m_selectSlots[i].gameObject.SetActive(false);
+        }
+    }
+
+
+    public void ResetSlot()
+    {
+        SetSlots();
+    }
+
+
+    #endregion
+}
+
---
+++ Assets/Scripts/UI/Slot/OneSelectSlot.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OneSelectSlot : SlotBase
+{
+    //아무기능없이 보여주기만 하는 슬랏
+    public TMP_Text selectedText; //나중에 아이콘으로 대체될부분
+    public TMP_Text selectedValue;
+    private UIOneByeSelect m_OneByeUI;
+    private int slotIndex;
+
+
+    public void SetSlot(TOrderItem _item, UIOneByeSelect _oneSelectUI, int _index)
+    {
+        m_OneByeUI = _oneSelectUI;
+        slotIndex = _index;
+        DetailSet(_item);
+    }
+
+    private void DetailSet(TOrderItem _item)
+    {
+        //보여주려는 아이템 타입에 따라 세팅하는거 
+        TokenType type = _item.Tokentype;
+        switch (type)
+        {
+            case TokenType.Conversation:
+                SetText(_item);
+                break;
+            default:
+                SetIcon(_item);
+                break;
+        }
+    }
+
+    private void SetText(TOrderItem _item)
+    {
+        ConversationData sentenece = MgMasterData.GetInstance().GetConversationData((ConversationEnum)_item.SubIdx, _item.Value);
+        selectedText.text = (slotIndex + 1).ToString();
+        selectedValue.text = sentenece.GetScript();
+    }
+
+    private void SetIcon(TOrderItem _item)
+    {
+        selectedText.text = ((Capital)_item.SubIdx).ToString();
+        selectedValue.text = _item.Value.ToString();
+    }
+
+
+    public override void OnLeftClick()
+    {
+        m_OneByeUI.OnSelectItem(slotIndex); //아이템 선택시
+    }
+
+}
