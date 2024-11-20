4ab9df9c31db4776c3fb2309ec623298db81c826
YiYop <wjdrbsdl1011@naver.com>
2024-11-20 20:17:24 +0900

    [추가] equipt 마스터 데이터 인스턴스 생성

--- Assets/Scripts/InGame/Equipment/EquiptItem.cs
+++ Assets/Scripts/InGame/Equipment/EquiptItem.cs
@@ -9,10 +9,15 @@ public enum EquiptEnum
 
 public class EquiptItem
 {
-    public TOrderItem m_effect; //플레이어 스텟에 가하는 요소
+    public List<TOrderItem> m_effect = new(); //플레이어 스텟에 가하는 요소
 
     public EquiptItem()
     {
-        m_effect = new TOrderItem(TokenType.CharStat, (int)CharStat.MaxHp, 30);
+        
+    }
+
+    public EquiptItem(List<int[]> matchCode, string[] valueCode)
+    {
+
     }
 }
