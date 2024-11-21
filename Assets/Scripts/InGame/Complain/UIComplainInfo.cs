89db992462c2ea4397ffc3a918a9e70c52e2f225
YiYop <wjdrbsdl1011@naver.com>
2024-11-21 21:41:15 +0900

    [추가] 컴플레인 정보 표기 유아이

---
+++ Assets/Scripts/InGame/Complain/UIComplainInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UIComplainInfo : UIBase
+{
+    Complain m_complain = null; //할당된 녀석
+
+    public void SetCompalinInfo(Complain _complain)
+    {
+        UISwitch(true);
+        m_complain = _complain;
+    }
+
+    //플레이어가 해결하기 누르면 
+    public void OnClickRespond()
+    {
+        m_complain.Respond();
+    }
+}
+
