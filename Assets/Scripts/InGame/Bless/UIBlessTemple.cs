4e28585d3a5a8a6793ab0340a8b3f26a113d53ec
YiYop <wjdrbsdl1011@naver.com>
2024-11-21 23:26:37 +0900

    [추가] 신전 UI 1.가호를 비는 부분과 얻는 부분을 분리 2. 가호를 얻을 때 Excutor로 집행 - Bool 반환받아서 추가조작 가능

--- Assets/Scripts/InGame/Bless/UIBlessTemple.cs
+++ Assets/Scripts/InGame/Bless/UIBlessTemple.cs
@@ -8,6 +8,7 @@ public class UIBlessTemple : UIBase
     //보유한 은총을 장착해제 가능한 곳 
     private GodClassEnum m_curTempleClass = GodClassEnum.무;
     private TokenTile m_curTile;
+    private GodBless newBless; //하사받은 은총
 
     public void SetTempleInfo(TokenTile _tile)
     {
@@ -18,14 +19,26 @@ public class UIBlessTemple : UIBase
 
     public void PleaseBlessBtn()
     {
-       GodBless bless =  MgGodBless.GetInstance().PleaseBless(m_curTempleClass);
-        if(bless == null)
+        newBless =  MgGodBless.GetInstance().PleaseBless(m_curTempleClass);
+        if(newBless == null)
         {
             Debug.Log("내릴 가호 없음");
             return;
         }
 
+    }
+
+    public void AquireBlessBtn()
+    {
+        if(newBless == null)
+        {
+            //받을 은총이 없다. 
+            return;
+        }
+
         //메인케릭터에 블레스 추가 
-        PlayerManager.GetInstance().GetMainChar().AddBless(bless);
+        TOrderItem blessItem = new TOrderItem(TokenType.Bless, newBless.PID, 0); //벨류는 머해야되나
+        OrderExcutor excutor = new();
+        excutor.AdaptItem(blessItem);
     }
 }
