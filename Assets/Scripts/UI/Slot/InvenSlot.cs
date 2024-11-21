1bc3625d1c8b44e79067f3be4ee89b20d97ba9dd
YiYop <wjdrbsdl1011@naver.com>
2024-11-22 00:10:23 +0900

    [추가]인벤용 슬랏 클래스

---
+++ Assets/Scripts/UI/Slot/InvenSlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class InvenSlot : SlotBase
+{
+    public TokenType m_tokenType; //할당된 아이템의 tokenType
+    public int m_itemPid = 0;
+    public TMP_Text m_itemName; //나중에 아이콘으로 대체될부분
+    public int m_slotIndex = 0;
+   
+    public void SetInvenSlot(GodBless _bless, int _slotIndex)
+    {
+        m_tokenType = TokenType.Bless;
+        m_itemPid = _bless.PID;
+        Naming();
+    }
+
+    public void SetInvenSlot(EquiptItem _equipt, int _slotIndex)
+    {
+        m_tokenType = TokenType.Equipt;
+        m_itemPid = _equipt.GetPid();
+        Naming();
+    }
+
+    private void Naming()
+    {
+        m_itemName.text = m_tokenType + " : " + m_itemPid;
+    }
+}
