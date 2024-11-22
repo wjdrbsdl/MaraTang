c4925734a4e6f39e24d7b928821eb7bda8f2f853
YiYop <wjdrbsdl1011@naver.com>
2024-11-22 17:32:18 +0900

    [수정] 변수명 수정

--- Assets/Scripts/InGame/Equipment/UIPlayerEquipt.cs
+++ Assets/Scripts/InGame/Equipment/UIPlayerEquipt.cs
@@ -16,22 +16,22 @@ public class UIPlayerEquipt : UIBase
     public void SetPlayerEquipt()
     {
         UISwitch(true);
-        List<EquiptItem> blessList = PlayerManager.GetInstance().GetMainChar().GetEquiptList();
+        List<EquiptItem> equiptList = PlayerManager.GetInstance().GetMainChar().GetEquiptList();
         //1. 아이템 수만큼 showSlot을 생성
-        MakeSamplePool<InvenSlot>(ref m_invenSlots, m_invenSlotSample.gameObject, blessList.Count, m_grid);
+        MakeSamplePool<InvenSlot>(ref m_invenSlots, m_invenSlotSample.gameObject, equiptList.Count, m_grid);
         //2. 리스트대로 슬랏에 표기
-        SetSlots(blessList);
+        SetSlots(equiptList);
     }
 
-    private void SetSlots(List<EquiptItem> _blessList)
+    private void SetSlots(List<EquiptItem> _equiptList)
     {
-        int itemCount = _blessList.Count;
+        int itemCount = _equiptList.Count;
 
         for (int i = 0; i < itemCount; i++)
         {
             int index = i;
             m_invenSlots[i].gameObject.SetActive(true);
-            m_invenSlots[i].SetInvenSlot(_blessList[i], index);
+            m_invenSlots[i].SetInvenSlot(_equiptList[i], index);
         }
         for (int i = itemCount; i < m_invenSlots.Length; i++)
         {
