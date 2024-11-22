f5166c63ad42daf95f5d69277111f2d7e4a49f60
YiYop <wjdrbsdl1011@naver.com>
2024-11-22 18:16:38 +0900

    [추가] 버리기 기능추가 1. 드롭된 아이템 토큰 확인, 타입확인해서 각 파트로 제거 호출

--- Assets/Scripts/UI/Slot/TrashZone.cs
+++ Assets/Scripts/UI/Slot/TrashZone.cs
@@ -10,5 +10,25 @@ public class TrashZone : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("뭔가 드롭이 되었따?");
+        
+        if (DragSlot.instance.dragslot != null)
+        {
+            TokenBase dragToken = DragSlot.instance.dragslot.GetTokenBase();
+            if (dragToken != null)
+                ThrowAway(dragToken);
+        }
+    }
+
+    private void ThrowAway(TokenBase _token)
+    {
+        TokenType tokenType = _token.GetTokenType();
+        TokenChar mainChar = PlayerManager.GetInstance().GetMainChar();
+        switch (tokenType)
+        {
+            case TokenType.Equipt:
+                mainChar.RemoveEquipt((EquiptItem)_token);
+                break;
+                
+        }
     }
 }
