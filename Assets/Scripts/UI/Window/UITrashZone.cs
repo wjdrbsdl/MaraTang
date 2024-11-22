8b45cbd864dac9bb7a049a0488d3ba23e4a75058
YiYop <wjdrbsdl1011@naver.com>
2024-11-22 17:55:20 +0900

    [추가] 트래쉬존 세팅 1. 세팅시 오브젝트 활성화 2. 세팅시 해당 UI 컨텐츠 transform에서 젤우선으로 - 젤 위에

--- Assets/Scripts/UI/Window/UITrashZone.cs
+++ Assets/Scripts/UI/Window/UITrashZone.cs
@@ -9,6 +9,8 @@ public class UITrashZone : MonoBehaviour
     public void SetTrashZone(GameObject _UI)
     {
         //해당 UI 오브젝트 하위 중 맨위로 위치하기 
+        gameObject.SetActive(true);
         gameObject.transform.SetParent(_UI.transform);
+        transform.SetAsFirstSibling();
     }
 }
