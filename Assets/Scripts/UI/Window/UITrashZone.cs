d509a77939d7dbc3e3249d2244cb8be50b996e1a
YiYop <wjdrbsdl1011@naver.com>
2024-11-22 17:43:57 +0900

    [추가] 휴지통 zone 클래스

---
+++ Assets/Scripts/UI/Window/UITrashZone.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITrashZone : MonoBehaviour
+{
+
+    public void SetTrashZone(GameObject _UI)
+    {
+        //해당 UI 오브젝트 하위 중 맨위로 위치하기 
+        gameObject.transform.SetParent(_UI.transform);
+    }
+}
