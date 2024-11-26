5b704544710a0eafd7b8b7b357c4609ca4183b37
YiYop <wjdrbsdl1011@naver.com>
2024-11-26 23:42:57 +0900

    [추가] 토큰타입에 UIOPen

--- Assets/Scripts/Token/TokenBase.cs
+++ Assets/Scripts/Token/TokenBase.cs
@@ -17,7 +17,7 @@ public enum OnChangeEnum
 public enum TokenType
 {
     Tile, Char, CharStat, Action, Player, Capital, Event, Nation, None, Content, NationTech, Conversation, NationStat, OnChange, Bless,
-    MonsterNationSpawn, EventPlaceNationSpawn, Equipt
+    MonsterNationSpawn, EventPlaceNationSpawn, Equipt, UIOpen
 }
 
 public class TokenBase
