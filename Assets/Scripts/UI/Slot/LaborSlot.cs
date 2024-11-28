17cba81da46e27a398040c4c1e0e939e9a850e11
YiYop <wjdrbsdl1011@naver.com>
2024-11-28 23:35:08 +0900

    [추가] 노동코인 슬랏

---
+++ Assets/Scripts/UI/Slot/LaborSlot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LaborSlot : SlotBase
+{
+    //아무기능없이 보여주기만 하는 슬랏
+    public TMP_Text laborNumText; //나중에 아이콘으로 대체될부분
+    [SerializeField]
+    private LaborCoin m_laborCoin; 
+
+    public void SetSlot(LaborCoin _laborCoin)
+    {
+        m_laborCoin = _laborCoin;
+        laborNumText.text = _laborCoin.ListIndex.ToString();
+    }
+
+}
