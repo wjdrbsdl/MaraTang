9c02d3d7475494651995d121c44fdb199530b28d
YiYop <wjdrbsdl1011@naver.com>
2024-12-01 17:29:42 +0900

    [제거] enum TileStat. 에너지 제거

--- Assets/Scripts/UI/Window/UITileInfo.cs
+++ Assets/Scripts/UI/Window/UITileInfo.cs
@@ -169,8 +169,8 @@ public class UITileInfo : UIBase
         MainResource mainResource = m_curTile.GetMainResource();
         TileType tileType = _tile.GetTileType();
         int NationNum = _tile.GetStat(TileStat.Nation);
-        string tileStat = string.Format("소속 국가 : {0}\n토지 용도 {1}\n 토지 적합도{2} 토지력 {3}\n좌표 {4},{5}\n노동 코인{6}",
-            NationNum, tileType, mainResource, _tile.GetStat(TileStat.TileEnergy), _tile.GetMapIndex()[0], _tile.GetMapIndex()[1], _tile.GetLaborCoinCount());
+        string tileStat = string.Format("소속 국가 : {0}\n토지 용도 {1}\n 토지 적합도{2}\n좌표 {3},{4}\n노동 코인{5}",
+            NationNum, tileType, mainResource, _tile.GetMapIndex()[0], _tile.GetMapIndex()[1], _tile.GetLaborCoinCount());
         m_statText.text = tileStat;
 
      
