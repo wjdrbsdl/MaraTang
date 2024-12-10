a22afa0f6e5d931f728df8d1dc89942088e80083
YiYop <wjdrbsdl1011@naver.com>
2024-12-10 19:58:57 +0900

    [추가] 자원 데이터 클래스

---
+++ Assets/Scripts/InGame/CapitalData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class CapitalData
+{
+    public Capital capital = Capital.Food;
+    public int Tier = 0;
+    public int baseRatio = 0; //기본 채집확률
+
+}
+
