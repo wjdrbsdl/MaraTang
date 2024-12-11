976d11af26346e68a09a4da37b785ed09fcc2021
YiYop <wjdrbsdl1011@naver.com>
2024-12-11 23:50:50 +0900

    [추가] 연구 UI, Slot

---
+++ Assets/Scripts/InGame/Nation/NationPolicy/ResearchSlot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum ReserchSlotFunction
+{
+    Select, Show
+}
+
+public class ResearchSlot : SlotBase
+{
+    public ReserchSlotFunction m_function = ReserchSlotFunction.Select; //해당 슬랏 기능 
+    private NationTechData m_techData; //할당된 스킬 pid
+    private bool m_isComplete = false; 
+    [SerializeField]
+    private UIResearch m_reserachUI;
+    public TMP_Text reserchText; //나중에 아이콘으로 대체될부분
+
+    public void SetResearchSlot(NationTechData _techData, bool _done)
+    {
+        gameObject.SetActive(true);
+        m_techData = _techData;
+        m_isComplete = _done;
+        reserchText.text = _techData.GetTechName();
+    }
+
+    public void OnClick()
+    {
+        if (m_function != ReserchSlotFunction.Select)
+            return;
+
+        //안 배운경우에만 배우겠다고 의사전달 
+        if (m_isComplete == false)
+            m_reserachUI.SelectReserach(m_techData);
+
+    }
+}
+
---
+++ Assets/Scripts/InGame/Nation/NationPolicy/UIResearch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class UIResearch : UIBase
+{
+    //해당 플레이어가 지닌 자원을 종류 수량을 선택하는 부분. 
+    [SerializeField]
+    private ResearchSlot m_reserachSlotSample;
+    [SerializeField]
+    private Transform m_grid;
+    [SerializeField]
+    private ResearchSlot[] m_researchSlots;
+
+    [SerializeField]
+    private ResearchSlot m_selectSlot;
+
+    public void SetNationResearch(int _nationNum)
+    {
+        UISwitch(true);
+        Nation nation = MgNation.GetInstance().GetNation(_nationNum);
+        List<NationTechData> techList = MgMasterData.GetInstance().GetTechDic().Select(dic => dic.Value).ToList();
+        //1. 아이템 수만큼 showSlot을 생성
+        MakeSamplePool<ResearchSlot>(ref m_researchSlots, m_reserachSlotSample.gameObject, techList.Count, m_grid);
+        //2. 리스트대로 슬랏에 표기
+        SetSlots(techList, nation.TechPart);
+    }
+
+    private void SetSlots(List<NationTechData> _techList, NationTechPart _nationTech)
+    {
+        int itemCount = _techList.Count;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            m_researchSlots[i].gameObject.SetActive(true);
+            m_researchSlots[i].SetResearchSlot(_techList[i], _nationTech.IsDoneTech(_techList[i].GetPid()));
+        }
+        for (int i = itemCount; i < m_researchSlots.Length; i++)
+        {
+            m_researchSlots[i].gameObject.SetActive(false);
+        }
+    }
+
+    public void SelectReserach(NationTechData _techData)
+    {
+
+    }
+}
+
