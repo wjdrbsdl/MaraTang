3b666c73b57a516a3006be4e343eaa05dfccbbd0
YiYop <wjdrbsdl1011@naver.com>
2024-12-13 21:08:16 +0900

    [추가] 대사 대기열 관리 1. 고유넘버와 대사를 관리 2. 대사 요청을 받으면 켤수 mgUI에게 문의 3. 기존 대사가 진행중이면 예약진행 4. 예약시 현재 대사와 같은 고유번호라면 0번째로 예약 5. 이외는 뒷순위로 예약 6. 대사창이 꺼지면서 다음 대사를 요청받으면 다시 반복

--- Assets/Scripts/Manager/DataManager/MGConversation.cs
+++ Assets/Scripts/Manager/DataManager/MGConversation.cs
@@ -5,47 +5,62 @@ using System;
 
 public class MGConversation : Mg<MGConversation>
 {
+    List<(int, ConversationData)> m_scriptList = new(); //표기되어야할 리스트 
+    int m_lastSerialNum = FixedValue.No_VALUE;
     public MGConversation()
     {
         g_instance = this;
-    }
-
-    public List<TOrderItem> GetSentenceItemList(ConversationThemeEnum _theme, int _start, int _end)
-    {
-        List<TOrderItem> sentenceList = new List<TOrderItem>();
-        for (int pid = _start; pid <= _end; pid++)
-        {
-            ConversationData conversation = GetConversationData(_theme, pid);
-            if (conversation == null)
-                continue;
-           // Debug.Log("뽑아온 대화 pid" + conversation.GetPid()+"요구한 pid "+pid);
-            TOrderItem sentenceItem = new TOrderItem(_theme, conversation);
-            sentenceList.Add(sentenceItem);
-        }
-
-        return sentenceList;
     }
 
     private ConversationData GetConversationData(ConversationThemeEnum _theme, int _pid)
     {
       return  MgMasterData.GetInstance().GetConversationData(_theme, _pid);
     }
-
-    //대화 선택창을 요구하고 싶으면 mgConversation으로 요청
-    public void ShowSelectScript(ConversationThemeEnum _theme, int _start, int _end, Action _confirmAction = null, ITradeCustomer _giver = null, ITradeCustomer _taker = null)
-    {
-        List<TOrderItem> senetenceItems = GetSentenceItemList(_theme, _start, _end);
-        SelectItemInfo selectInfo = new SelectItemInfo(senetenceItems, true, 1, 1); //대사 선택은 1개만 
-        selectInfo.SetAction(_confirmAction);
-        selectInfo.SetGiver(_giver);
-        selectInfo.SetTaker(_taker);
-        selectInfo.ShowScript();
-    }
     
-    public void ShowCheckScript(TOrderItem _scriptItem, int _serialNum)
+    public void ShowScriptItem(TOrderItem _scriptItem, int _serialNum)
     {
         ConversationData scriptData = GetConversationData((ConversationThemeEnum)_scriptItem.SubIdx, _scriptItem.Value);
-        MgUI.GetInstance().ShowScript(scriptData, _serialNum);
+        ShowScript(scriptData, _serialNum);
+    }
+
+    private void ShowScript(ConversationData _script, int _serialNum)
+    {
+        if (MgUI.GetInstance().ShowScript(_script, _serialNum) == false)
+        {
+            //대화를 열어달라고 요청 받았는데, 대화를 열지 못했을 경우 
+            ReservateScript(_serialNum, _script);//예약하고 종료
+            return;
+        }
+        //대화창에 잘 출력했으면
+        m_lastSerialNum = _serialNum; //마지막 출력했던 고유번호 저장
+    }
+
+    public void RequestNextScrtip()
+    {
+      //  Debug.Log("예약 진행 전 남은 대사 수 " + m_scriptList.Count);
+        if(m_scriptList.Count == 0)
+        {
+            return;
+        }
+
+       // Debug.Log("0번째 대사 출력 ");
+        ShowScript(m_scriptList[0].Item2, m_scriptList[0].Item1);
+        m_scriptList.RemoveAt(0);
+      //  Debug.Log("출력 후 남은 대사 수 " + m_scriptList.Count);
+    }
+
+    private void ReservateScript(int _serialNum, ConversationData _scriptData)
+    {
+        //마지막 진행했던 대화와 같은 맥락의 대화면 0번째로 추가
+     //   Debug.LogFormat("{0}번 고유번호로 식별번호:{1}, 대사 :{2} 예약", _serialNum, _scriptData.GetPid(), _scriptData.GetScript());
+        if (m_lastSerialNum == _serialNum)
+        {
+            m_scriptList.Insert(0, (_serialNum, _scriptData));
+            return;
+        }
+        //이외에는 맨 뒷 순서로 
+        m_scriptList.Add((_serialNum, _scriptData)); //해당 대화를 씨리얼넘버와 함께 추가
+     //   Debug.Log("예약된 카운트 " + m_scriptList.Count);
     }
 }
 
