55373188e7c2d9af235c9a1f32613c30bba717c9
YiYop <wjdrbsdl1011@naver.com>
2024-12-13 21:06:56 +0900

    [추가] 대사 누적 진행위해 구조 수정 1. UIScript에 따로 확인버튼 생성 2. 확인버튼 누르면 확인 진행 3. 퀘스트 성공 여부 연계로 다음 대사가 요청 4. 요청된 대사는 기존 창이 닫히지 않았기 때문에 예약됨 5. 이후 대사창 종료 6. 종료하면서 override에서 대화MG에게 다음 대사를 요청 호출

--- Assets/Scripts/UI/Window/UIScript.cs
+++ Assets/Scripts/UI/Window/UIScript.cs
@@ -11,11 +11,39 @@ public class UIScript : UISelectBase
 
     public void SetScript(ConversationData _scriptData, int _serialNum)
     {
+      //  Debug.Log(_serialNum + "대사 출력");
         UISwitch(true);
         string script = _scriptData.GetScript();
         m_scriptText.text = script;
         m_selectInfo = new SelectItemInfo(null, false, 0, 0, _serialNum); ;
         m_serialNum = _serialNum;
+    }
+
+    public void OnConfirmScript()
+    {
+
+        bool ableConfirm = m_selectInfo.AbleConfirm();
+        //컨펌 반려 먹었으면 빠꾸
+        if (ableConfirm == false)
+            return;
+        
+        //취소로 다른 스크립이 호출 되는 경우 새로 SetScript()로 세팅된 스크립트 창이 종료되버림
+        if (m_selectInfo != null)
+            m_selectInfo.Confirm();
+
+        //퀘스트 연계로 다음 대화가 호출될때 해당 UI가 켜져있기 때문에 예약에 걸림. 
+
+        m_selectInfo = null; //초기화 진행
+
+        //끄면서 mg대사에 다음 대화를 요청 
+        UISwitch(false);
+    }
+
+    public override void OffWindow()
+    {
+        base.OffWindow();//스택에서 뺴면서
+        Debug.Log("다음 대사 요청");
+        MGConversation.GetInstance().RequestNextScrtip();
     }
 
 }
