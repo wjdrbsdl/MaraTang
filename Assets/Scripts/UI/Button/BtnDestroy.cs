672cdc20477d64a5a6cebcc46e88a33b5fa6bfee
YiYop <wjdrbsdl1011@naver.com>
2024-12-19 21:46:26 +0900

    [추가] 파괴 버튼 1. 파괴 작업서 만들어서 제출

---
+++ Assets/Scripts/UI/Button/BtnDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnDestroy : MonoBehaviour
+{
+    private TokenTile m_tile;
+    private UIBase m_motherUI;
+
+    public void SetButton(TokenTile _tile, UIBase _motherUI)
+    {
+        m_tile = _tile;
+        m_motherUI = _motherUI;
+    }
+    public void OnClickDestroy()
+    {
+        TileTypeData tileData = MgMasterData.GetInstance().GetTileData((int)m_tile.GetTileType());
+        TokenTile destroyTile = m_tile;
+        WorkOrder work = new WorkOrder(tileData.DestroyCostData.GetItemList(), tileData.NeedDestroyTurn, 0, destroyTile, 0, WorkType.DestroyPlace);
+        if (work.DoneWrite)
+        {
+            Debug.Log("파괴 요청 진행");
+        }
+    }
+
+}
