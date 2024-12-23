24cb848d0f4901d6bc6aceaab4d6851185b708fe
YiYop <wjdrbsdl1011@naver.com>
2024-12-23 18:40:57 +0900

    [추가] 국가 영지 관리 파트 클래스, 최대 영지보유수 공식

---
+++ Assets/Scripts/InGame/Nation/NationCategory/NationTerritoryPart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NationTerritoryPart
+{
+    private Nation m_nation;
+   
+    public NationTerritoryPart(Nation _nation)
+    {
+        m_nation = _nation;
+    }
+
+    public int GetMaxTerritoryCount()
+    {
+        //최대 영지 = 레벨 * 레벨당 토지 + 추가 토지
+        int maxCount;
+
+        int level = m_nation.GetNationLevel();
+        int countByLevel = m_nation.GetStat(NationStatEnum.TerritoryByLevel);
+        int addCount = m_nation.GetStat(NationStatEnum.AddTerritory);
+
+        maxCount = (level * countByLevel) + addCount;
+
+        return maxCount;
+    }
+
+  
+}
