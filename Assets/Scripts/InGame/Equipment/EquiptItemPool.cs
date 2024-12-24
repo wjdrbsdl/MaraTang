194ceda1d40244e2dd5201a9c988d94de6183c4f
YiYop <wjdrbsdl1011@naver.com>
2024-12-24 21:05:51 +0900

    [추가] 장비 옵션풀 저장해놓은 클래스

---
+++ Assets/Scripts/InGame/Equipment/EquiptItemPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public enum EquiptPartEnum
+{
+    None, Weapon, Armor, Shoese, Gloves, Helmet
+}
+
+public class EquiptItemPool
+{
+    public int m_pid = 0;
+    public TokenType m_tokenType = TokenType.Equipt;
+    public string m_itemName = "";
+    public EquiptPartEnum m_part = EquiptPartEnum.None;
+    public List<TOrderItem> m_effect = new(); //플레이어 스텟에 가하는 요소
+    public List<int> m_optionPoolList = new();
+
+
+    public EquiptItemPool()
+    {
+        
+    }
+
+    public EquiptItemPool(string[] _dbValueList)
+    {
+        m_pid = int.Parse(_dbValueList[0]);
+        m_tokenType = TokenType.Equipt;
+        m_itemName = _dbValueList[1];
+
+        int partIdx = 2;
+        if (System.Enum.TryParse(typeof(EquiptPartEnum), _dbValueList[partIdx], out object parsePart))
+            m_part = (EquiptPartEnum)parsePart;
+
+        int optionIdx = partIdx + 1;
+        string[] options = _dbValueList[optionIdx].Split(FixedValue.PARSING_LIST_DIVIDE);
+        for (int i = 0; i < options.Length; i++)
+        {
+            string pid = options[i];
+            if(int.TryParse(pid, out int varidPid))
+            {
+                m_optionPoolList.Add(varidPid);
+            }
+        }
+    }
+
+}
