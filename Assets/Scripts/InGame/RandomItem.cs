26d45a2ee459e655949014f08c26267f8f1192b1
YiYop <wjdrbsdl1011@naver.com>
2024-12-25 01:16:36 +0900

    [수정] 랜덤 다이스 전용 클래스 생성 1. randomSubIdx - 랜덤 타입에 따라 2. 해당 Toritem Value를 이용하여 적절한 TOrderItem을 새로생성하여 반환 3. 자원 1티어면 1티어 자원중 반환 4. 장비면 장비 아이템중 하나 반환 5. 장비 옵션을 돌려서 장비를 생성해서 반환기능도 추가

--- Assets/Scripts/InGame/RandomItem.cs
+++ Assets/Scripts/InGame/RandomItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using System;
 
 public enum RandomTypeEnum
 {
@@ -11,4 +12,68 @@ public enum RandomTypeEnum
 public class RandomItem
 {
  
+
+    public TOrderItem GetDiceItem(TOrderItem _item)
+    {
+        TOrderItem item = new TOrderItem(TokenType.None, 0, 0);
+        RandomTypeEnum randomType = (RandomTypeEnum)_item.SubIdx;
+        switch (randomType)
+        {
+            case RandomTypeEnum.EquiptAllPart:
+                //장비 모든파트중 하나를 반환
+                return item;
+            case RandomTypeEnum.Capital1:
+                int tier = 1;
+                int amount = _item.Value;
+                return GetDiceCapital(tier, amount);
+        }
+
+        return item;
+    }
+
+    private TOrderItem GetDiceCapital(int _tier, int _amount)
+    {
+        TOrderItem noneItem = new TOrderItem(TokenType.None, 0, 0);
+        int amount = _amount; //레벨에 맞는 자원들을 훑어와야함. 
+        List<Capital> randomList = new();
+        Array capital = System.Enum.GetValues(typeof(Capital));
+        for (int i = 1; i < capital.Length; i++)
+        {
+            //0 인덱스는 None이므로 1부터 진행 
+            Capital test = (Capital)capital.GetValue(i);
+            CapitalData capitalData = MgMasterData.GetInstance().GetCapitalData(test);
+            if (capitalData == null)
+            {
+                if (FixedValue.CAPITAL_NONE_ARALM == false)
+                {
+                    Debug.LogError("없는 데이터 토큰타입" + test);
+                    FixedValue.CAPITAL_NONE_ARALM = true;
+                }
+
+                continue;
+            }
+
+            if (capitalData.Tier == _tier)
+            {
+                randomList.Add((Capital)capitalData.capital);
+            }
+        }
+        if (randomList.Count == 0)
+            return noneItem;
+
+        Capital selectCapital = randomList[GameUtil.GetRandomNum(randomList.Count, 1)[0]];
+        return new TOrderItem(TokenType.Capital, (int)selectCapital, amount);
+        
+    }
+
+    public TOrderItem GetDiceEquiptOption(TOrderItem _equiptItem)
+    {
+        TOrderItem equiptItem = _equiptItem;
+        int poolPid = _equiptItem.SubIdx;
+        EquiptItemPool equiptPool = MgMasterData.GetInstance().GetEquiptPoolData(poolPid); //풀 정보 가져옴
+        int tier = _equiptItem.Value;
+        EquiptItem equipt = equiptPool.GetItem(tier);
+        equiptItem.tokenItem = equipt;
+        return equiptItem;
+    }
 }
