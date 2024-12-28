47a6a314ecdd315fb559bbe48790af1cee43f902
YiYop <wjdrbsdl1011@naver.com>
2024-12-28 20:01:03 +0900

    [추가] 시너지 효과 변수 할당

--- Assets/Scripts/InGame/Bless/BlessSynerge.cs
+++ Assets/Scripts/InGame/Bless/BlessSynerge.cs
@@ -13,13 +13,16 @@ public class BlessSynerge
     public int PID;
     public string Name;
     List<TOrderItem> m_needBlessList; //시너지 활성화에 필요한 가호 조건 리스트
-
+    List<TOrderItem> m_effectList; 
     public BlessSynerge(string[] _divdeValues)
     {
         PID = int.Parse( _divdeValues[0]);
         Name = _divdeValues[2];
         m_needBlessList = new();
         GameUtil.ParseOrderItemList(m_needBlessList, _divdeValues[3]);
+
+        m_effectList = new();
+        GameUtil.ParseOrderItemList(m_effectList, _divdeValues[4]);
         //서브 인덱스가 클수록 더 좁은 범위 먼저 확인해야하므로 앞으로 배치 
         m_needBlessList.Sort((TOrderItem A, TOrderItem B) => B.SubIdx.CompareTo(A.SubIdx));
     }
