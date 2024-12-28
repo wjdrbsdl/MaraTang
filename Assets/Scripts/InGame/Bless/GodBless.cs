92e70bf074e2f06fe24e2aaac793e40684f17ada
YiYop <wjdrbsdl1011@naver.com>
2024-12-28 19:54:36 +0900

    [제거] 신은총 변수 1. 신 enum 제거 -> 신 pid로 int로 받아서 진행 2. 은총에서 신을 파싱하지않고, 해당 은총을 지니는 신이 알아서 할당

--- Assets/Scripts/InGame/Bless/GodBless.cs
+++ Assets/Scripts/InGame/Bless/GodBless.cs
@@ -8,15 +8,10 @@ public enum GodClassEnum
     무, 마법, 전사, 궁사, 여정
 }
 
-public enum BlessSubCategory
-{
-    전사_A,전사_B,전사_C,전사_1,전사_2
-}
-
 public class GodBless : TokenBase
 {
-    public GodClassEnum m_mainCategory = GodClassEnum.무; //가호 시너지 체크를 위해 분류 
-    public BlessSubCategory m_subCategory = BlessSubCategory.전사_A; //신의 이름
+    public GodClassEnum m_classCategory = GodClassEnum.무; //가호 시너지 체크를 위해 분류 
+    public int m_godPid = 0; //신의 이름
     public List< TOrderItem> m_effect; //플레이어 스텟에 가하는 요소
 
     public GodBless(string[] _dbValueList)
@@ -24,9 +19,7 @@ public class GodBless : TokenBase
         m_tokenPid = int.Parse(_dbValueList[0]);
         m_tokenType = TokenType.Bless;
         m_itemName = _dbValueList[1];
-        int subPartIdx = 2;
-        if (System.Enum.TryParse(typeof(BlessSubCategory), _dbValueList[subPartIdx], out object parsePart))
-            m_subCategory = (BlessSubCategory)parsePart;
+     
         m_effect = new();
         GameUtil.ParseOrderItemList(m_effect, _dbValueList[3]);
 
@@ -42,8 +35,8 @@ public class GodBless : TokenBase
         m_tokenPid  = _origin.m_tokenPid ;
         m_tokenType = _origin.m_tokenType;
         m_itemName = _origin.m_itemName;
-        m_mainCategory = _origin.m_mainCategory;
-        m_subCategory = _origin.m_subCategory;
+        m_classCategory = _origin.m_classCategory;
+        m_godPid = _origin.m_godPid;
         m_effect = new();
         for (int i = 0; i < _origin.m_effect.Count; i++)
         {
