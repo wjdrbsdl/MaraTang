aa155c3ec94ddeec1e4e1359cf14f0abab5cf732
YiYop <wjdrbsdl1011@naver.com>
2024-12-30 21:04:59 +0900

    [추가] 자원 마스터데이터에 값 추가 1. 기본 채취 량 2. 발견 가능땅

--- Assets/Scripts/InGame/CapitalData.cs
+++ Assets/Scripts/InGame/CapitalData.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using UnityEngine;
 
 public class CapitalData
 {
-    public int capital = (int)Capital.None;
+    public int capitalPid = (int)Capital.None;
     public int Tier = 0;
     public int baseRatio = 0; //기본 채집확률
-
-
+    public int baseAmount = 0; //기본 채집수량
+    public TileType ablePlace = TileType.None; //발견 가능장소
     public CapitalData(List<int[]> matchCode, string[] _parsingData)
     {
         int capitalPidIndex = 0;
         if (System.Enum.TryParse(typeof(Capital), _parsingData[capitalPidIndex], out object parseTileType))
-            capital = (int)parseTileType;
+            capitalPid = (int)parseTileType;
 
         Tier = int.Parse(_parsingData[2]);
         baseRatio = int.Parse(_parsingData[3]);
-        
+
+        if (System.Enum.TryParse(typeof(TileType), _parsingData[capitalPidIndex], out object ableTileType))
+            ablePlace = (TileType)ableTileType;
+
+        Debug.Log((Capital)capitalPid + "자원 발견 가능장소 " + ablePlace);
+        baseAmount = int.Parse(_parsingData[5]);
+
+
     }
 }
 
