dfaf073aaa59082ae96f55838bb4dc0c5f223882
YiYop <wjdrbsdl1011@naver.com>
2024-12-30 21:08:13 +0900

    [추가] 자원 랜덤 티어 채집 방식 1. 티어는 일단 제외 - 임시로 n티어 캔다고 호출 2. 랜덤이 들어갔을 경우 해당 티어에서 자원 pool 생성 뽑을 자원 선택 3. 선택된 자원의 기본 수량 적용 해서 채집 가능성 진행

--- Assets/Scripts/InGame/DiceRandomItem.cs
+++ Assets/Scripts/InGame/DiceRandomItem.cs
@@ -6,7 +6,7 @@ using System;
 
 public enum RandomTypeEnum
 {
-    Capital1, EquiptAllPart
+    Capital1, EquiptAllPart, Capital
 }
 
 public class DiceRandomItem
@@ -26,6 +26,9 @@ public class DiceRandomItem
                 int tier = 1;
                 int amount = _item.Value;
                 return GetDiceCapital(tier, amount);
+            case RandomTypeEnum.Capital:
+                int itemtier = _item.Value; //
+                return GetDiceCapital(itemtier);
         }
 
         return item;
@@ -55,7 +58,7 @@ public class DiceRandomItem
 
             if (capitalData.Tier == _tier)
             {
-                randomList.Add((Capital)capitalData.capital);
+                randomList.Add((Capital)capitalData.capitalPid);
             }
         }
         if (randomList.Count == 0)
@@ -64,6 +67,43 @@ public class DiceRandomItem
         Capital selectCapital = randomList[GameUtil.GetRandomNum(randomList.Count, 1)[0]];
         return new TOrderItem(TokenType.Capital, (int)selectCapital, amount);
         
+    }
+
+    private TOrderItem GetDiceCapital(int _tier)
+    {
+        TOrderItem noneItem = new TOrderItem(TokenType.None, 0, 0);
+
+        List<Capital> randomList = new();
+        Array capital = System.Enum.GetValues(typeof(Capital));
+        for (int i = 1; i < capital.Length; i++)
+        {
+            //0 인덱스는 None이므로 1부터 진행 
+            Capital test = (Capital)capital.GetValue(i);
+            CapitalData capitalData = MgMasterData.GetInstance().GetCapitalData(test);
+            if (capitalData == null)
+            {
+                if (FixedValue.CAPITAL_NONE_ARALM == false)
+                {
+                    Debug.LogError("없는 데이터 토큰타입" + test);
+                    FixedValue.CAPITAL_NONE_ARALM = true;
+                }
+
+                continue;
+            }
+
+            if (capitalData.Tier == _tier)
+            {
+                randomList.Add((Capital)capitalData.capitalPid);
+            }
+        }
+        if (randomList.Count == 0)
+            return noneItem;
+
+        Capital selectCapital = randomList[GameUtil.GetRandomNum(randomList.Count, 1)[0]];
+        CapitalData capitalDataf = MgMasterData.GetInstance().GetCapitalData(selectCapital);
+        Debug.Log((Capital)capitalDataf.capitalPid + "의 기본 채집수량 " + capitalDataf.baseAmount);
+        return new TOrderItem(TokenType.Capital, (int)selectCapital, capitalDataf.baseAmount);
+
     }
 
     public TOrderItem GetDiceEquiptOption(TOrderItem _equiptItem)
