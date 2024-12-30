0deabbe73dad07bedcafc09bccdcc2007ac31363
YiYop <wjdrbsdl1011@naver.com>
2024-12-30 22:47:29 +0900

    [수정] 장비 pid 리스트 public

--- Assets/Scripts/InGame/Fight/DropItemManager.cs
+++ Assets/Scripts/InGame/Fight/DropItemManager.cs
@@ -8,7 +8,7 @@ public class DropItemManager : Mg<DropItemManager>
 {
     //동적으로 보상받을 아이템들이 정해졌고, 적용 혹은 선택을 기다릴 때 오는 매니저
     //로딩시 선택부터 진행해야하는 경우 이곳에 체크를 해놓고, 아이템 리스트나 연결시킬 컨텐츠등을 기록해놓는다. 
-    private List<int> equiptPidList;
+    public List<int> equiptPidList;
 
     #region 초기화
     public DropItemManager()
