bd31f51ad06df912c9829ac54e5a04d895264f8e
YiYop <wjdrbsdl1011@naver.com>
2024-12-31 22:41:25 +0900

    [추가] 버프 클래스 정보 1. 버프 enum 2. 버프 중첩 type enum 3. 버프 기본 정보 무슨 버프인가, 이름, 중첩타입, 효과

--- Assets/Scripts/InGame/Battle/TokenBuff.cs
+++ Assets/Scripts/InGame/Battle/TokenBuff.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 using System;
 
 
+public enum BuffEnum
+{
+    Fracture, ArmorBreak
+}
+
+public enum NestingType
+{
+    Cant, Reset, Stack
+}
+
 public class TokenBuff : TokenBase
 {
+    public BuffEnum m_buff = BuffEnum.Fracture;
+    public NestingType m_nestType = NestingType.Cant;
+    public List<TOrderItem> m_effect = new();
 
     #region 버프 생성부분
     public TokenBuff()
@@ -15,29 +28,25 @@ public class TokenBuff : TokenBase
     }
 
     //마스터 캐릭데이터 생성
-    public TokenBuff(List<int[]> matchCode, string[] valueCode)
+    public TokenBuff(string[] valueCode)
     {
-        m_tokenPid = int.Parse(valueCode[0]); //시트 데이터상 0번째는 pid
+        if (System.Enum.TryParse(typeof(BuffEnum), valueCode[0], out object parsebuff))
+        {
+            m_tokenPid = (int)parsebuff;
+            m_buff = (BuffEnum)parsebuff;
+        }
+
         m_itemName = valueCode[1]; //1은 이름
-
-        int charTypeIndex = 2;
-        //if (System.Enum.TryParse(typeof(CharType), valueCode[charTypeIndex], out object charType))
-        //    m_charType = (CharType)charType;
-
-        int tierIndex = charTypeIndex + 1;
-        if (int.TryParse(valueCode[tierIndex], out int tier))
-            m_tier = tier;
-
-
-
+        m_tokenType = TokenType.Buff;
     }
     //복사본 캐릭 생성 : 캐릭터 스폰 시 사용
     public TokenBuff(TokenBuff _masterToken)
     {
         m_tokenPid = _masterToken.m_tokenPid;
+        m_buff = _masterToken.m_buff;
         m_itemName = _masterToken.m_itemName;
-        m_tokenType = TokenType.Char;
-     
+        m_tokenType = _masterToken.m_tokenType;
+
         int arraySize = _masterToken.m_tokenIValues.Length;
         m_tokenIValues = new int[arraySize];
         //마스터 데이터 깊은 복사로 객체 고유 배열 값 생성. 
