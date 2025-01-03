9d16cff42100656e4bc2c3e0c0ddb99e3e8b6a3b
YiYop <wjdrbsdl1011@naver.com>
2025-01-03 16:41:40 +0900

    [수정] 옵션의 각 단계에 필요한 티어와 파워로 데이터 설정 1. 옵션마다 단계가 있고, 각 단계는 충족하는 티어를 조건으로 한다. 2. 매칭되는 index로 그 단계의 효과 수치 power를 정한다. 3. 해당 옵션에서 벨류를 뽑을때 주어진 티어를 가지고 충족하는 단계를 모두 나열하고 4. 나열된 단계중에서 적용할 단계를 뽑는다. 5. 뽑은 단계에 해당하는 파워를 최종 옵션수치로 결정한다. 6. startTier는 해당 옵션이 등장 가능한지 비교를 위한 값으로 활용된다.

--- Assets/Scripts/InGame/Equipment/EquiptOptionData.cs
+++ Assets/Scripts/InGame/Equipment/EquiptOptionData.cs
@@ -8,8 +8,8 @@ public class EquiptOptionData
     public int Pid;
     public string Name;
     public int SubIdx; //각 토큰타입에 해당하는 SubIdx 보통 CharStat으로 적용될듯
-    public int Tier1Value; //1티어 시작점 + Gap이 1티어 구간
-    public int TierGap; //각 티어 폭
+    public List<int> TierStepList; //1티어 시작점 + Gap이 1티어 구간
+    public List<int> TierPowerList; //각 티어 폭
     public int PoolDiceValue; //해당 옵션 가중치 - 높을수록 여러 옵션중 잘뜸
     public int StartTier; //해당 옵션이 발생가능한 Tier
 
@@ -19,18 +19,29 @@ public class EquiptOptionData
         int pidIdx = 0;
         int nameIdx = pidIdx + 1;
         int subvalueIdx = nameIdx + 1;
-        int tier1valueIdx = subvalueIdx + 1;
-        int tierGapIdx = tier1valueIdx + 1;
-        int pooldiceIdx = tierGapIdx + 1;
-        int startTierIdx = pooldiceIdx + 1;
+        int tierStepIdx = subvalueIdx + 1;
+        int powerIdx = tierStepIdx + 1;
+        int pooldiceIdx = powerIdx + 1;
+     
         Pid = int.Parse(_parsingData[pidIdx]);
         Name = _parsingData[nameIdx];
         if (System.Enum.TryParse(typeof(CharStat), _parsingData[subvalueIdx], out object sub))
             SubIdx = (int)sub;
-        Tier1Value = int.Parse(_parsingData[tier1valueIdx]);
-        TierGap = int.Parse(_parsingData[tierGapIdx]);
+        string[] stepStr = _parsingData[tierStepIdx].Split(FixedValue.PARSING_LINE_DIVIDE);
+        TierStepList = new();
+        string[] powerStr = _parsingData[powerIdx].Split(FixedValue.PARSING_LINE_DIVIDE);
+        TierPowerList = new();
+        for (int i = 0; i < stepStr.Length; i++)
+        {
+            int step = int.Parse(stepStr[i]);
+            int power = int.Parse(powerStr[i]);
+            TierStepList.Add(step);
+            TierPowerList.Add(power);
+        }
         PoolDiceValue = int.Parse(_parsingData[pooldiceIdx]);
-        StartTier = int.Parse(_parsingData[startTierIdx]);
+        StartTier = 1;
+        if (TierStepList.Count >= 1)
+            StartTier = TierStepList[0]; //맨 처음단긴애가 제일 최초 시작 티어 
 
     }
 
@@ -44,10 +55,27 @@ public class EquiptOptionData
             return new TOrderItem(TokenType.None, 0, 0);
         }
 
-        //다음 시작점은 gap+1 씩이네
-        int effectMinValue = Tier1Value + (TierGap + 1) * (adaptTier - 1); //해당 티어 에서 최소 값
-       // Debug.Log(_tier + "티어의 의 최소 값은" + effectMinValue);
-        TOrderItem effect = new TOrderItem(TokenType.CharStat, SubIdx, effectMinValue);
+        //티어 스텝부터 시작해서 현재 티어 사이의 티어를 선출
+        //선출된 티어중 하나 선택
+        //선택된 티어의 벨류값으로 power 선택
+        int ableStepCount = 1; // 뽑기할 스텝의 총수 
+        for (int i = 1; i < TierStepList.Count; i++)
+        {
+            if (TierStepList[i] <= dropTier)
+            {
+                ableStepCount += 1; //단계 적용 티어 값이 드롭티어보다 낮으면 해당 단계도 가능이므로 가능 단계 +1
+            }
+            else
+            {
+                //아니라면 최대 스텝 찾기 종료
+                break;
+            }
+        }
+
+        int stepdice = Random.Range(0, ableStepCount); //가능한 단계수에서 뽑기시작 - 만약 1단계만 가능하면 0만 나와서 0번째 수치가 적용될것
+        int selectPower = TierPowerList[stepdice];
+        Debug.Log(Name + " 적용 "+ (stepdice + 1) + "단계 파워 적용 인덱스는 " + stepdice + " 효과 수치는 " + selectPower);
+        TOrderItem effect = new TOrderItem(TokenType.CharStat, SubIdx, selectPower);
         return effect;
     }
 }
