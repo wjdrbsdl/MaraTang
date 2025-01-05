9b6eff57f6a39e98faeaf765724b01465b8c611c
YiYop <wjdrbsdl1011@naver.com>
2025-01-05 22:51:44 +0900

    [추가] 민원 발생 구조 1. 발생 민원 수 결정 2. 발생 민원 결정 3. 발생 영지 결정

--- Assets/Scripts/InGame/Complain/ComplainManager.cs
+++ Assets/Scripts/InGame/Complain/ComplainManager.cs
@@ -34,42 +34,67 @@ public class ComplainManager : Mg<ComplainManager>
 
     bool isDebugWarning = false; //ToDo 한번 알림용 변수
 
-   public Complain OccurComplain(TokenTile _tile)
+    //국가가 전달 되면 해당 국가에서 발생할 민원을 설정
+    public void OccurComplain(Nation _nation)
     {
-        Nation nation = _tile.GetNation();
-        //해당 장소에서 발생가능한 민원과 그 확률을 다른 스텟과 조율하여 민원 반환
-        Complain occurComplain = null; //만드는순간 겜마스터에게 등록됨. 
-        //분류된 민원을 가지고, 새로운 민원으로 복사해서 반환 
-        if(isDebugWarning == false)
+        if (isDebugWarning == false)
         {
             Debug.LogWarning("임시로 불만 제로");
             isDebugWarning = true;
         }
+
         
-        return null;
+        //1. 민원 발생 수 - 일정 비율에 토지 수를 곱해서 결정 - 일정비율은 변화가능 분위기좋을땐 높은게 좋고, 나쁠땐 낮은게 좋고
+        int occurCount = CalComplainCount(_nation);
+        //2. 발생 민원 결정
+        List<Complain> occurComplainList = FindComplainList(_nation, occurCount);
+        //3. 민원 영토에 배정
+        AssignComplain(_nation, occurComplainList);
+       
     }
 
-    //국가가 전달 되면 해당 국가에서 발생할 민원을 설정
-    public void OccurComplain(Nation _nation)
+    private int CalComplainCount(Nation _nation)
     {
-        List<TokenTile> territory = _nation.GetTerritorry();
-        for (int i = 1; i < territory.Count; i++) //수도는 제외 1 부터 index
-        {
-            TokenTile tile = territory[i];
-            if (tile.HaveComplain())
-            {
-                continue;
-            }
-            Complain complain = compleMg.OccurComplain(tile);
-            if (complain == null)
-            {
-                continue;
-            }
-            //임시로 여기서 좌표 새김, Complain 생성자에서 매개변수로 받은 위치로 생성하는게 좋아보임. 
-            complain.SetComplainMapIndex(tile.GetMapIndex());
-            tile.SendComplain(complain);
+        int territoryCount = _nation.GetTerritorry().Count;
+        int tempOccurRatio = 30;
+        decimal accurateRatio = tempOccurRatio * 0.01m;
+        int occurCount = (int)(territoryCount * accurateRatio);
+        Debug.Log("정밀 비율" + accurateRatio + " 발생 수치" + occurCount);
+        return occurCount;
+    }
 
+    private List<Complain> FindComplainList(Nation nation, int _occurCount)
+    {
+        //민원 발생에 관련될 스텟을 정의
+        List<Complain> complainList = new();
+        for (int i = 1; i <= _occurCount; i++)
+        {
+            //민원 발생수만큼 민원을 발생
+            Complain newComplain = new Complain();
+            complainList.Add(newComplain); //
         }
+        return complainList;
+    }
+
+    private void AssignComplain(Nation _nation, List<Complain> _complaintList)
+    {
+        
+        List<TokenTile> territory = _nation.GetTerritorry();
+        //민원을 국가 영지에 적절하게 배분할것. 
+        for (int complainIdx = 0; complainIdx < _complaintList.Count; complainIdx++)
+        {
+            Complain complaint = _complaintList[complainIdx];
+            for (int i = 1; i < territory.Count; i++) //수도는 제외 1 부터 index
+            {
+                TokenTile tile = territory[i];
+                if (tile.HaveComplain())
+                {
+                    continue;
+                }
+                complaint.AssignTile(tile);
+            }
+        }
+        
     }
 }
 
