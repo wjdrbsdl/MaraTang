702da2b122a8facd7f3a6ef8f656c514f3240a1a
YiYop <wjdrbsdl1011@naver.com>
2025-01-06 20:07:53 +0900

    [추가] 민원 정보표기 요청타입과 남은턴

--- Assets/Scripts/InGame/Complain/UIComplainInfo.cs
+++ Assets/Scripts/InGame/Complain/UIComplainInfo.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+using TMPro;
 public class UIComplainInfo : UIBase
 {
     Complain m_complain = null; //할당된 녀석
-
+    public TMP_Text InfoText;
     public void SetCompalinInfo(Complain _complain)
     {
         UISwitch(true);
         m_complain = _complain;
+        InfoText.text = "요청 타입 " + _complain.RequestType + "남은 턴 "+_complain.RestTurn;
     }
 
     //플레이어가 해결하기 누르면 
